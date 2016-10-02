9519f63a3e9da138bd101e6eb69220ab50441bd2
Jarred Capellman <jcapellman@hotmail.com>
2016-10-01 20:35:28 -0400

    Removed js/map files

--- Src/jcIDS.RESTService/config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var config;
-config = {
-    REDIS_DATABASE_HOSTNAME: 'localhost',
-    REDIS_DATABASE_PORT: 6379,
-    HTTP_SERVER_PORT: 1338
-};
-module.exports = config;
-//# sourceMappingURL=config.js.map
