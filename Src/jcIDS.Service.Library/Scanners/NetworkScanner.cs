e82098755071d3a875ac8c3467e204c414d2a4c7
Jarred Capellman <jcapellman@hotmail.com>
2016-10-07 21:27:12 -0400

    Added Analyzer project for Neural Network

--- Src/jcIDS.Service.Library/Scanners/NetworkScanner.cs
+++ Src/jcIDS.Service.Library/Scanners/NetworkScanner.cs
@@ -1,11 +1,14 @@
-﻿using jcIDS.Service.Library.Objects;
+﻿using System.Net;
+using System.Net.Sockets;
+using jcIDS.Service.Library.Objects;
 
 namespace jcIDS.Service.Library.Scanners {
     public class NetworkScanner : BaseScanner {
         public NetworkScanner(ScannerWrapperItem scannerWrapper) : base(scannerWrapper) { }
 
-        public override void Run() {
-            
+        public override void Run()
+        {
+           
         }
 
         public override string Name() => "Network";
