8d01e904cf209cbab0557535fddb3ce996c96558
Jarred Capellman <jcapellman@hotmail.com>
2018-08-18 16:35:20 -0500

    Added: Initial projects

---
+++ src/jcIDS.service.app/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace jcIDS.service.app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
