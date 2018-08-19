73cb26ec770e57d5e3573d436d91a8225166a8a6
Jarred Capellman <jcapellman@hotmail.com>
2018-08-19 08:26:00 -0500

    Added: Initial work on the Core Manager class

---
+++ src/jcIDS.library.core/Managers/CoreManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using jcIDS.library.core.Common;
+
+namespace jcIDS.library.core.Managers
+{
+    public class CoreManager
+    {
+        public void Initialize()
+        {
+            Console.WriteLine(Constants.APP_NAME);
+        }
+    }
+}
