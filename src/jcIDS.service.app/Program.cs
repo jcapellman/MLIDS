fa3d1e40e631ccbbf37f097e83c95c30ca484e0d
Jarred Capellman <jcapellman@hotmail.com>
2018-08-19 19:28:21 -0500

    Added: Linux Style command shutdown

--- src/jcIDS.service.app/Program.cs
+++ src/jcIDS.service.app/Program.cs
@@ -1,14 +1,50 @@
-﻿using jcIDS.library.core.Managers;
+﻿using System;
+using System.Runtime.Loader;
+
+using jcIDS.library.core.Managers;
+
+using NLog;
 
 namespace jcIDS.service.app
 {
     class Program
     {
+        private static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
+        private static CoreManager _coreManager;
+
         static void Main(string[] args)
         {
-            var coreManager = new CoreManager();
+            AssemblyLoadContext.Default.Unloading += Default_Unloading;
 
-            coreManager.Initialize();
+            Console.CancelKeyPress += Console_CancelKeyPress;
+
+            _coreManager = new CoreManager();
+
+            if (!_coreManager.Initialize())
+            {
+                return;
+            }
+
+            _coreManager.StartService();
+        }
+
+        private static void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e)
+        {
+            Log.Info("Service Canceled");
+
+            _coreManager?.StopService();
+
+            Log.Info("Succesfully Canceled");
+        }
+
+        private static void Default_Unloading(AssemblyLoadContext obj)
+        {
+            Log.Info("Service unloading");
+
+            _coreManager?.StopService();
+
+            Log.Info("Succesfully Unloaded");
         }
     }
 }
