152f639f82707b518eed58ab74b16c80b37dd614
Jarred Capellman <jcapellman@hotmail.com>
2018-08-19 19:29:39 -0500

    Added: Initial work on the LicenseManager

---
+++ src/jcIDS.library.core/Managers/LicenseManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace jcIDS.library.core.Managers
+{
+    public class LicenseManager
+    {
+        public bool IsRegistered()
+        {
+            return true;
+        }
+    }
+}
