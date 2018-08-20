c1e579da5d64f5b816fd48243662df6e136dee05
Jarred Capellman <jcapellman@hotmail.com>
2018-08-19 19:30:00 -0500

    Added: Initial work on the WhiteListManager class

---
+++ src/jcIDS.library.core/Managers/WhiteListManager.cs
@@ -0,0 +1,12 @@
+﻿using jcIDS.library.core.Interfaces;
+
+namespace jcIDS.library.core.Managers
+{
+    public class WhiteListManager : IListManager
+    {
+        public bool IsContained(string resourceItem)
+        {
+            return false;
+        }
+    }
+}
