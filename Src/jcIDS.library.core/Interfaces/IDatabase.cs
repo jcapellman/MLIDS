982ffb90475b44de70abf8689ec77be5c7cf5d3d
Jarred Capellman <jcapellman@hotmail.com>
2018-09-11 20:35:40 -0500

    Added: Initial work on the IDatabase class

---
+++ Src/jcIDS.library.core/Interfaces/IDatabase.cs
@@ -0,0 +1,11 @@
+﻿using jcIDS.library.core.DAL.Objects.Base;
+
+namespace jcIDS.library.core.Interfaces
+{
+    interface IDatabase
+    {
+        bool DeleteItem<T>(T item) where T: BaseObject;
+
+        bool AddItem<T>(T item) where T : BaseObject;
+    }
+}
