7389f963dc638632aca0b1bd43dfea41df3c2f0e
Jarred Capellman <jcapellman@hotmail.com>
2018-09-11 20:34:26 -0500

    Added: Initial work on the LiteDBDAL class

---
+++ src/jcIDS.library.core/DAL/LiteDBDAL.cs
@@ -0,0 +1,30 @@
+﻿using jcIDS.library.core.DAL.Objects.Base;
+using jcIDS.library.core.Interfaces;
+
+namespace jcIDS.library.core.DAL
+{
+    public class LiteDBDAL : IDatabase
+    {
+        private const string FILENAME = "main.db";
+
+        public bool AddItem<T>(T item) where T : BaseObject
+        {
+            using (var db = new LiteDB.LiteDatabase(FILENAME))
+            {
+                var collection = db.GetCollection<T>();
+
+                return collection.Insert(item) > 0;
+            }
+        }
+
+        public bool DeleteItem<T>(T item) where T : BaseObject
+        {
+            using (var db = new LiteDB.LiteDatabase(FILENAME))
+            {
+                var collection = db.GetCollection<T>();
+
+                return collection.Delete(item.ID);
+            }
+        }
+    }
+}
