203edb5434b22c922c1d38eb214a4548a886f612
Jarred Capellman <jcapellman@hotmail.com>
2018-09-11 20:35:12 -0500

    Added: Initial work on the BlackListObject class

---
+++ src/jcIDS.library.core/DAL/Objects/BlackListObject.cs
@@ -0,0 +1,9 @@
+﻿using jcIDS.library.core.DAL.Objects.Base;
+
+namespace jcIDS.library.core.DAL.Objects
+{
+    public class BlackListObject : BaseObject
+    {
+        public string ResourceName { get; set; }
+    }
+}
