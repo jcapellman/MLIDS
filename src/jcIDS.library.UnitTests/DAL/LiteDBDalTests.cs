c97d272cc77acf09695cf1b4d908ec0f582dcbce
Jarred Capellman <jcapellman@hotmail.com>
2018-09-13 22:06:27 -0500

    Added: Additional Unit Tests to cover the LiteDBDal

--- src/jcIDS.library.UnitTests/DAL/LiteDBDalTests.cs
+++ src/jcIDS.library.UnitTests/DAL/LiteDBDalTests.cs
@@ -22,5 +22,75 @@ namespace jcIDS.library.UnitTests.DAL
 
             Assert.IsNull(val);
         }
+
+        [TestMethod]
+        public void ContainsNullItem()
+        {
+            var litedb = new LiteDBDAL();
+
+            var contains = litedb.Contains<TestObject>(null);
+
+            Assert.IsFalse(contains);
+        }
+
+        [TestMethod]
+        public void ContainsMiss()
+        {
+            var litedb = new LiteDBDAL();
+
+            var contains = litedb.Contains<TestObject>(new TestObject { ID = 0 });
+
+            Assert.IsFalse(contains);
+        }
+
+        [TestMethod]
+        public void ContainsHit()
+        {
+            var litedb = new LiteDBDAL();
+
+            var result = litedb.AddItem(new TestObject());
+
+            Assert.IsTrue(result);
+
+            var contains = litedb.Contains(new TestObject { ID = 1 });
+
+            Assert.IsTrue(contains);
+        }
+
+        [TestMethod]
+        public void DeleteNull()
+        {
+            var litedb = new LiteDBDAL();
+
+            var result = litedb.DeleteItem<TestObject>(null);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void DeleteMiss()
+        {
+            var litedb = new LiteDBDAL();
+
+            var result = litedb.DeleteItem<TestObject>(new TestObject());
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void DeleteHit()
+        {
+            var litedb = new LiteDBDAL();
+
+            litedb.AddItem(new TestObject());
+
+            var item = litedb.GetItem<TestObject>(1);
+
+            Assert.IsNotNull(item);
+
+            var result = litedb.DeleteItem(item);
+
+            Assert.IsFalse(result);
+        }
     }
 }
