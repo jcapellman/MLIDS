bc3e20cb25f66479c858affc489f9f8b9f1663f1
Jarred Capellman <jcapellman@hotmail.com>
2018-09-13 21:00:27 -0500

    Added: Initial work on Unit Tests

---
+++ src/jcIDS.library.UnitTests/DAL/LiteDBDalTests.cs
@@ -0,0 +1,26 @@
+﻿using jcIDS.library.core.DAL;
+using jcIDS.library.core.DAL.Objects.Base;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace jcIDS.library.UnitTests.DAL
+{
+    [TestClass]
+    public class LiteDBDalTests
+    {
+        public class TestObject : BaseObject
+        {
+
+        }
+
+        [TestMethod]
+        public void GetNullItem()
+        {
+            var litedb = new LiteDBDAL();
+
+            var val = litedb.GetItem<TestObject>(0);
+
+            Assert.IsNull(val);
+        }
+    }
+}
