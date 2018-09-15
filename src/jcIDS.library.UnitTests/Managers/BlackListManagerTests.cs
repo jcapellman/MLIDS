a061def1e04a565b142cb59ebda3c641da873cf0
Jarred Capellman <jcapellman@hotmail.com>
2018-09-14 21:33:27 -0500

    Added: Initial work on the BlackListManagerTests class

---
+++ src/jcIDS.library.UnitTests/Managers/BlackListManagerTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using jcIDS.library.core.Managers;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace jcIDS.library.UnitTests.Managers
+{
+    [TestClass]
+    public class BlackListManagerTests
+    {
+        [TestInitialize]
+        public void Setup()
+        {
+            var coreManager = new CoreManager();
+
+            coreManager.Initialize();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ContainsNull()
+        {
+            var blackListManager = new BlackListManager();
+
+            blackListManager.IsContained(null);   
+        }
+
+        [TestMethod]
+        public void ContainsMiss()
+        {
+            var blackListManager = new BlackListManager();
+
+            var result = blackListManager.IsContained("Test");
+
+            Assert.IsFalse(result);
+        }
+    }
+}
