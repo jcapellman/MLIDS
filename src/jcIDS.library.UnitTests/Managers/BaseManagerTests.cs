35a1d21092a8770572dde89c669ba63cf20e602c
Jarred Capellman <jcapellman@hotmail.com>
2018-09-15 08:30:00 -0500

    Added: Initial checkin of the BaseManagerTests class

---
+++ src/jcIDS.library.UnitTests/Managers/BaseManagerTests.cs
@@ -0,0 +1,18 @@
+﻿using jcIDS.library.core.Managers;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace jcIDS.library.UnitTests.Managers
+{
+    [TestClass]
+    public class BaseManagerTests
+    {
+        [TestInitialize]
+        public void Setup()
+        {
+            var coreManager = new CoreManager();
+
+            coreManager.Initialize();
+        }
+    }
+}
