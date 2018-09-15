ff8ef5a75aca13bb67c18deba6e1893a80a850d0
Jarred Capellman <jcapellman@hotmail.com>
2018-09-15 10:00:19 -0500

    Added: Expression Support in the IListManager class

--- Src/jcIDS.library.core/Interfaces/IListManager.cs
+++ Src/jcIDS.library.core/Interfaces/IListManager.cs
@@ -1,12 +1,15 @@
-﻿using jcIDS.library.core.DAL.Objects.Base;
+﻿using System;
+using System.Linq.Expressions;
+
+using jcIDS.library.core.DAL.Objects.Base;
 
 namespace jcIDS.library.core.Interfaces
 {
     interface IListManager<T> where T : BaseObject
     {
-        bool IsContained(string resourceName);
+        bool IsContained(Expression<Func<T, bool>> expression);
 
-        T GetItem(int id);
+        T GetItem(Expression<Func<T, bool>> expression);
 
         int AddItem(T item);
     }
