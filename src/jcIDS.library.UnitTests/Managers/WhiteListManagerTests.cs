1c9e13f6c788fa4633d2abf0ff609924326f6f1e
Jarred Capellman <jcapellman@hotmail.com>
2018-09-15 09:43:09 -0500

    Added: Initial work on the WhiteListManagerTests class

---
+++ src/jcIDS.library.UnitTests/Managers/WhiteListManagerTests.cs
@@ -0,0 +1,56 @@
+﻿using jcIDS.library.core.DAL.Objects;
+using jcIDS.library.core.Managers;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace jcIDS.library.UnitTests.Managers
+{
+    [TestClass]
+    public class WhiteListManagerTests : BaseManagerTests
+    {
+        [TestMethod]
+        public void ContainsNull()
+        {
+            var whiteListManager = new WhiteListManager();
+
+            Assert.IsFalse(whiteListManager.IsContained(null));
+        }
+
+        [TestMethod]
+        public void ContainsMiss()
+        {
+            var whiteListManager = new WhiteListManager();
+
+            var result = whiteListManager.IsContained("Test");
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void GetNullItem()
+        {
+            var whiteListManager = new WhiteListManager();
+
+            var result = whiteListManager.GetItem(0);
+
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void GetActualItem()
+        {
+            var whiteListManager = new WhiteListManager();
+
+            var item = new WhiteListObject
+            {
+                ResourceName = "Test"
+            };
+
+            var resultID = whiteListManager.AddItem(item);
+
+            var result = whiteListManager.GetItem(resultID);
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
