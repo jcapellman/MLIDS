dd7e38ff3de9549b52a0cedec63862413801a914
Jarred Capellman <jcapellman@hotmail.com>
2018-09-15 10:01:01 -0500

    Added: Expression Queries to the WhiteListManager

--- src/jcIDS.library.core/Managers/WhiteListManager.cs
+++ src/jcIDS.library.core/Managers/WhiteListManager.cs
@@ -1,16 +1,15 @@
-﻿using jcIDS.library.core.DAL.Objects;
+﻿using System;
+using System.Linq.Expressions;
+using jcIDS.library.core.DAL.Objects;
 using jcIDS.library.core.Interfaces;
 
 namespace jcIDS.library.core.Managers
 {
     public class WhiteListManager : IListManager<WhiteListObject>
     {
-        public bool IsContained(string resourceName) => CoreManager.GetService<IDatabase>().Contains(new WhiteListObject
-        {
-            ResourceName = resourceName
-        });
+        public bool IsContained(Expression<Func<WhiteListObject, bool>> expression) => CoreManager.GetService<IDatabase>().Contains(expression);
 
-        public WhiteListObject GetItem(int id) => CoreManager.GetService<IDatabase>().GetItem<WhiteListObject>(id);
+        public WhiteListObject GetItem(Expression<Func<WhiteListObject, bool>> expression) => CoreManager.GetService<IDatabase>().GetItem(expression);
 
         public int AddItem(WhiteListObject item) => CoreManager.GetService<IDatabase>().AddItem(item);
     }
