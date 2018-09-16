b1a1c1d906926d81da88e5a0f62dc0329df9fe09
Jarred Capellman <jcapellman@hotmail.com>
2018-09-16 09:14:54 -0500

    Added: Initial work on the NetworkDeviceObject class #4

---
+++ src/jcIDS.library.core/DAL/Objects/NetworkDeviceObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using jcIDS.library.core.DAL.Objects.Base;
+
+namespace jcIDS.library.core.DAL.Objects
+{
+    public class NetworkDeviceObject : BaseObject
+    {
+        public string IPV4Address { get; set; }
+
+        public string IPV6Address { get; set; }
+
+        public string MAC { get; set; }
+
+        public DateTime LastOnline { get; set; }
+    }
+}
