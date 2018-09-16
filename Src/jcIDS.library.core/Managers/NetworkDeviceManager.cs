12d69f40c6dc4a193b74b758c16ad6d0d7abfa42
Jarred Capellman <jcapellman@hotmail.com>
2018-09-16 09:15:12 -0500

    Added: Initial work on the NetworkDeviceManager class #4

---
+++ Src/jcIDS.library.core/Managers/NetworkDeviceManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq.Expressions;
+
+using jcIDS.library.core.DAL.Objects;
+using jcIDS.library.core.Interfaces;
+
+namespace jcIDS.library.core.Managers
+{
+    public class NetworkDeviceManager : IListManager<NetworkDeviceObject>
+    {
+        public bool IsContained(Expression<Func<NetworkDeviceObject, bool>> expression) => CoreManager.GetService<IDatabase>().Contains(expression);
+
+        public NetworkDeviceObject GetItem(Expression<Func<NetworkDeviceObject, bool>> expression) => CoreManager.GetService<IDatabase>().GetItem(expression);
+
+        public int AddItem(NetworkDeviceObject item) => CoreManager.GetService<IDatabase>().AddItem(item);
+    }
+}
