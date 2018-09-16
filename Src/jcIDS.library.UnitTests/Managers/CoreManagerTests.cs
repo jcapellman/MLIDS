be0e0114853dbda8ace3fe307b7bf37147d99a63
Jarred Capellman <jcapellman@hotmail.com>
2018-09-16 08:52:28 -0500

    Added: Initial work on the CoreManagerTests

---
+++ Src/jcIDS.library.UnitTests/Managers/CoreManagerTests.cs
@@ -0,0 +1,24 @@
+﻿using jcIDS.library.core.Managers;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace jcIDS.library.UnitTests.Managers
+{
+    [TestClass]
+    public class CoreManagerTests
+    {
+        [TestMethod]
+        public void StartStopService()
+        {
+            var coreManager = new CoreManager();
+
+            var result = coreManager.Initialize();
+
+            Assert.IsTrue(result);
+
+            coreManager.StartService();
+
+            coreManager.StopService();
+        }
+    }
+}
