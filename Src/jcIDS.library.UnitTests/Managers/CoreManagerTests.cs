757e67c5aecb1019a576db50148679a2805dee05
Jarred Capellman <jcapellman@hotmail.com>
2018-09-23 10:01:36 -0500

    Change: Updated the CoreManager Test to be more precise

--- Src/jcIDS.library.UnitTests/Managers/CoreManagerTests.cs
+++ Src/jcIDS.library.UnitTests/Managers/CoreManagerTests.cs
@@ -8,17 +8,13 @@ namespace jcIDS.library.UnitTests.Managers
     public class CoreManagerTests
     {
         [TestMethod]
-        public void StartStopService()
+        public void Initialize()
         {
             var coreManager = new CoreManager();
 
             var result = coreManager.Initialize();
 
             Assert.IsTrue(result);
-
-            coreManager.StartService();
-
-            coreManager.StopService();
         }
     }
 }
