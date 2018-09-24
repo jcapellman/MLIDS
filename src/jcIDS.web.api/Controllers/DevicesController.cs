4126c0e9f0785bf3f5fb11a56d7787a133a7382b
Jarred Capellman <jcapellman@hotmail.com>
2018-09-23 19:42:26 -0500

    Added: Initial commit of the Web API

---
+++ src/jcIDS.web.api/Controllers/DevicesController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+using jcIDS.library.core.DAL.Objects;
+using jcIDS.library.core.Managers;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace jcIDS.web.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DevicesController : ControllerBase
+    {
+        private readonly NetworkDeviceManager _deviceManager;
+
+        public DevicesController(NetworkDeviceManager deviceManager)
+        {
+            _deviceManager = deviceManager;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<NetworkDeviceObject>> Get()
+        {
+            return _deviceManager.GetAll();
+        }
+    }
+}
---
+++ src/jcIDS.web.api/Startup.cs
@@ -0,0 +1,51 @@
+﻿using jcIDS.library.core.Managers;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace jcIDS.web.api
+{
+    public class Startup
+    {
+        private CoreManager _coreManager;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            _coreManager = new CoreManager();
+
+            if (!_coreManager.Initialize())
+            {
+                return;
+            }
+
+            services.AddSingleton(new NetworkDeviceManager());
+        }
+        
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
