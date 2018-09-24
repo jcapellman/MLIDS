8a4834fe6d218a698d82ad283608fdbf83705b54
Jarred Capellman <jcapellman@hotmail.com>
2018-09-23 19:36:11 -0500

    Added: GetAll implementation to the BlackListManager class

--- Src/jcIDS.library.core/Managers/BlackListManager.cs
+++ Src/jcIDS.library.core/Managers/BlackListManager.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq.Expressions;
 
 using jcIDS.library.core.DAL.Objects;
@@ -13,5 +14,7 @@ namespace jcIDS.library.core.Managers
         public BlackListObject GetItem(Expression<Func<BlackListObject, bool>> expression) => CoreManager.GetService<IDatabase>().GetItem(expression);
 
         public int AddItem(BlackListObject item) => CoreManager.GetService<IDatabase>().AddItem(item);
+
+        public List<BlackListObject> GetAll() => CoreManager.GetService<IDatabase>().GetAll<BlackListObject>();
     }
 }
