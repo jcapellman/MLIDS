5fb7e510761709315cdc614c6ca0e02ce0ed8849
Jarred Capellman <jcapellman@hotmail.com>
2018-09-24 20:28:25 -0500

    Added: Initial work on the BlackListController

---
+++ src/jcIDS.web.api/Controllers/BlackListController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+using jcIDS.library.core.DAL.Objects;
+using jcIDS.library.core.Managers;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace jcIDS.web.api.Controllers
+{
+    public class BlackListController : BaseController
+    {
+        private readonly BlackListManager _blackListManager;
+
+        public BlackListController(BlackListManager blackListManager)
+        {
+            _blackListManager = blackListManager;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<BlackListObject>> Get()
+        {
+            return _blackListManager.GetAll();
+        }
+    }
+}
