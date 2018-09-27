1a781dd9e6ff45f1b1e50a5d297739a5bcf85f50
Jarred Capellman <jcapellman@hotmail.com>
2018-09-26 20:23:51 -0500

    Change: Started cleaning up Android code

--- src/jcIDS.Mobile/jcIDS.Mobile/ViewModels/ItemDetailViewModel.cs
+++ src/jcIDS.Mobile/jcIDS.Mobile/ViewModels/ItemDetailViewModel.cs
@@ -1,12 +1,11 @@
-﻿using System;
-
-using jcIDS.Mobile.Models;
+﻿using jcIDS.Mobile.Models;
 
 namespace jcIDS.Mobile.ViewModels
 {
     public class ItemDetailViewModel : BaseViewModel
     {
         public Item Item { get; set; }
+
         public ItemDetailViewModel(Item item = null)
         {
             Title = item?.Text;
