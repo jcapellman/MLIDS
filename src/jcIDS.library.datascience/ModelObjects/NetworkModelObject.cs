1fc942536b6ca1ee641305578af87c95337ae9be
Jarred Capellman <jcapellman@hotmail.com>
2018-09-29 18:58:32 -0500

    Added: Initial work on the NetworkModelObject class

---
+++ src/jcIDS.library.datascience/ModelObjects/NetworkModelObject.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.ML.Runtime.Api;
+
+namespace jcIDS.library.datascience.ModelObjects
+{
+    public class NetworkModelObject
+    {
+        [Column("0")]
+        public string IPAddress { get; set; }
+    }
+}
