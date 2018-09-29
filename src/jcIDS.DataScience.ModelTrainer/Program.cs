c01e80e3baeeed32d074c7319b13d5d59ac7a342
Jarred Capellman <jcapellman@hotmail.com>
2018-09-29 18:57:31 -0500

    Added: Further work on the ModelTrainer app

--- src/jcIDS.DataScience.ModelTrainer/Program.cs
+++ src/jcIDS.DataScience.ModelTrainer/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 
+using jcIDS.library.datascience.Managers;
+
 namespace jcIDS.DataScience.ModelTrainer
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Specify the LiteDB filename to train on");
+
+                return;
+            }
+
+            var result = new TrainerManager().TrainModel(args[0]);
+
+            Console.WriteLine(result ? "Model trained successfully" : "Model Training failed");
         }
     }
 }
