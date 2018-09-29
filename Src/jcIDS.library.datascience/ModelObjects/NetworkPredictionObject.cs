de0ec497a6b4fef02bb743808f0f6f1d4b499a0f
Jarred Capellman <jcapellman@hotmail.com>
2018-09-29 18:59:42 -0500

    Added: Initial work on the NetworkPredictionObject class

---
+++ Src/jcIDS.library.datascience/ModelObjects/NetworkPredictionObject.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.ML.Runtime.Api;
+
+namespace jcIDS.library.datascience.ModelObjects
+{
+    public class NetworkPredictionObject
+    {
+        [ColumnName("PredictedLabel")]
+        public string PredictedLabels;
+    }
+}
