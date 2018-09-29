1a95d5db7260ed0b8d31ba61404ed222eb89a78d
Jarred Capellman <jcapellman@hotmail.com>
2018-09-29 18:30:41 -0500

    Added: Initial checkin of the ModelTrainer app

---
+++ src/jcIDS.DataScience.ModelTrainer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace jcIDS.DataScience.ModelTrainer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
