61775b4a10943f9f2a74f145420dd25abfb970fe
Jarred Capellman <jcapellman@hotmail.com>
2018-09-30 11:39:25 -0500

    Added: ToCSV Extension Method for BaseObjects

---
+++ src/jcIDS.library.core/Common/ExtensionMethods.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+
+using jcIDS.library.core.DAL.Objects.Base;
+
+namespace jcIDS.library.core.Common
+{
+    public static class ExtensionMethods
+    {
+        public static string ToCSV(this BaseObject baseObject)
+        {
+            var properties = baseObject.GetType().GetProperties();
+
+            var values = properties.Select(property => property.GetValue(baseObject).ToString()).ToList();
+
+            return string.Join(",", values);
+        }
+    }
+}
