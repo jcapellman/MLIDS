06c3b4e7723730e46583c35b739a3ef5e03a26e1
Jarred Capellman <jcapellman@hotmail.com>
2018-09-30 19:09:48 -0500

    Added: Implementation of the Initialize method in the LiteDBDAL Change: Swapped out Filename with DI Config

--- src/jcIDS.library.core/DAL/LiteDBDAL.cs
+++ src/jcIDS.library.core/DAL/LiteDBDAL.cs
@@ -1,19 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq.Expressions;
 
+using jcIDS.library.core.Common;
 using jcIDS.library.core.DAL.Objects.Base;
 using jcIDS.library.core.Interfaces;
+using jcIDS.library.core.Managers;
 
 namespace jcIDS.library.core.DAL
 {
     public class LiteDBDAL : IDatabase
     {
-        private const string FILENAME = "main.db";
-
         public int AddItem<T>(T item) where T : BaseObject
         {
-            using (var db = new LiteDB.LiteDatabase(FILENAME))
+            using (var db = new LiteDB.LiteDatabase(CoreManager.GetService<Config>().DBFileName))
             {
                 var collection = db.GetCollection<T>();
 
@@ -23,7 +24,7 @@ namespace jcIDS.library.core.DAL
 
         public bool Contains<T>(Expression<Func<T, bool>> expression) where T : BaseObject
         {
-            using (var db = new LiteDB.LiteDatabase(FILENAME))
+            using (var db = new LiteDB.LiteDatabase(CoreManager.GetService<Config>().DBFileName))
             {
                 return db.GetCollection<T>().FindOne(expression) != null;
             }
@@ -31,7 +32,7 @@ namespace jcIDS.library.core.DAL
 
         public T GetItem<T>(Expression<Func<T, bool>> expression) where T : BaseObject
         {
-            using (var db = new LiteDB.LiteDatabase(FILENAME))
+            using (var db = new LiteDB.LiteDatabase(CoreManager.GetService<Config>().DBFileName))
             {
                 var collection = db.GetCollection<T>();
 
@@ -41,7 +42,7 @@ namespace jcIDS.library.core.DAL
 
         public bool DeleteAll<T>() where T : BaseObject
         {
-            using (var db = new LiteDB.LiteDatabase(FILENAME))
+            using (var db = new LiteDB.LiteDatabase(CoreManager.GetService<Config>().DBFileName))
             {
                 return db.DropCollection(typeof(T).Name);
             }
@@ -49,7 +50,7 @@ namespace jcIDS.library.core.DAL
 
         public bool UpdateItem<T>(Expression<Func<T, bool>> expression, T item) where T : BaseObject
         {
-            using (var db = new LiteDB.LiteDatabase(FILENAME))
+            using (var db = new LiteDB.LiteDatabase(CoreManager.GetService<Config>().DBFileName))
             {
                 return db.GetCollection<T>().Update(item);
             }
@@ -57,15 +58,25 @@ namespace jcIDS.library.core.DAL
 
         public List<T> GetAll<T>() where T : BaseObject
         {
-            using (var db = new LiteDB.LiteDatabase(FILENAME))
+            using (var db = new LiteDB.LiteDatabase(CoreManager.GetService<Config>().DBFileName))
             {
                 return new List<T>(db.GetCollection<T>().FindAll());
             }
         }
 
+        public bool Initialize()
+        {
+            if (!File.Exists(CoreManager.GetService<Config>().DBFileName))
+            {
+                throw new FileNotFoundException("Could not find LiteDB File", CoreManager.GetService<Config>().DBFileName);
+            }
+
+            return true;
+        }
+
         public bool DeleteItem<T>(T item) where T : BaseObject
         {
-            using (var db = new LiteDB.LiteDatabase(FILENAME))
+            using (var db = new LiteDB.LiteDatabase(CoreManager.GetService<Config>().DBFileName))
             {
                 if (item == null)
                 {
