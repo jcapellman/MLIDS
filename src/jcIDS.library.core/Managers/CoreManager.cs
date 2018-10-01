cd5ad56a2383efdba762979c8ede477e88cdca80
Jarred Capellman <jcapellman@hotmail.com>
2018-09-30 19:10:49 -0500

    Added: GetConfig to parse the config file

--- src/jcIDS.library.core/Managers/CoreManager.cs
+++ src/jcIDS.library.core/Managers/CoreManager.cs
@@ -1,12 +1,14 @@
 ﻿using System;
+using System.IO;
 using System.Threading;
 
+using jcIDS.library.core.Common;
 using jcIDS.library.core.DAL;
 using jcIDS.library.core.Interfaces;
 using jcIDS.library.core.PlatformInterfaces;
 
 using Microsoft.Extensions.DependencyInjection;
-
+using Newtonsoft.Json;
 using NLog;
 
 using NetworkInterface = jcIDS.library.core.PlatformImplementations.NetworkInterface;
@@ -23,7 +25,28 @@ namespace jcIDS.library.core.Managers
 
         public static T GetService<T>() => _container.GetService<T>();
 
-        public bool Initialize()
+        private Config GetConfig(string configFileName)
+        {
+            if (!File.Exists(configFileName))
+            {
+                _log.Warn($"Could not find {configFileName}, using default config...");
+
+                return new Config();
+            }
+
+            try
+            {
+                return JsonConvert.DeserializeObject<Config>(File.ReadAllText(configFileName));
+            }
+            catch (JsonException ex)
+            {
+                _log.Warn($"Could not parse Config ({configFileName}) due to {ex}, using default config...");
+            }
+
+            return new Config();
+        }
+
+        public bool Initialize(string configFileName = Constants.DEFAULT_FILENAME_CONFIG)
         {
             if (!_licenseManager.IsRegistered())
             {
@@ -31,8 +54,12 @@ namespace jcIDS.library.core.Managers
 
                 return false;
             }
-            
+
+            var config = GetConfig(configFileName);
+
             var serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddSingleton(config);
 
             serviceCollection.AddTransient(typeof(IDatabase), typeof(LiteDBDAL));
 
