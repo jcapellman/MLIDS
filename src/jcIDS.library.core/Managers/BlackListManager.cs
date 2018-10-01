f84061227ad450523fbdfd4fec0999f71c042468
Jarred Capellman <jcapellman@hotmail.com>
2018-09-30 19:27:05 -0500

    Added: Files that were stored in Src vs src

---
+++ src/jcIDS.library.core/Managers/BlackListManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+using jcIDS.library.core.DAL.Objects;
+using jcIDS.library.core.Interfaces;
+
+namespace jcIDS.library.core.Managers
+{
+    public class BlackListManager : IListManager<BlackListObject>
+    {
+        public bool IsContained(Expression<Func<BlackListObject, bool>> expression) => CoreManager.GetService<IDatabase>().Contains(expression);
+
+        public BlackListObject GetItem(Expression<Func<BlackListObject, bool>> expression) => CoreManager.GetService<IDatabase>().GetItem(expression);
+
+        public int AddItem(BlackListObject item) => CoreManager.GetService<IDatabase>().AddItem(item);
+
+        public List<BlackListObject> GetAll() => CoreManager.GetService<IDatabase>().GetAll<BlackListObject>();
+        public string ToCSV()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/jcIDS.library.core/PlatformInterfaces/INetworkInterfaces.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using jcIDS.library.core.DAL.Objects;
+
+namespace jcIDS.library.core.PlatformInterfaces
+{
+    public interface INetworkInterfaces
+    {
+        bool IsOnline();
+
+        List<NetworkDeviceObject> ScanDevices();
+
+        string DeviceIPAddress { get; }
+
+        NetworkInterface GetNetworkInterface();
+    }
+}
