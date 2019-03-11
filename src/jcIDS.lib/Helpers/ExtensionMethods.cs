926d6f12c3fb025850a260a457392b2f2100f8d6
Jarred Capellman <jcapellman@hotmail.com>
2019-03-11 09:25:56 +0000

    Added: ExtensionMethods class

---
+++ src/jcIDS.lib/Helpers/ExtensionMethods.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Sockets;
+
+namespace jcIDS.lib.Helpers
+{
+    public static class ExtensionMethods
+    {
+        public static ProtocolType ToProtocolType(this byte value)
+        {
+            switch (value)
+            {
+                case 1:
+                    return ProtocolType.Icmp;
+                case 2:
+                    return ProtocolType.Igmp;
+                case 6:
+                    return ProtocolType.Tcp;
+                case 17:
+                    return ProtocolType.Udp;
+                default:
+                    return ProtocolType.Unknown;
+            }
+        }
+    }
+}
