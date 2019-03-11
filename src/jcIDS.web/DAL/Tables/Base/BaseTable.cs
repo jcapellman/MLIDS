c9831fc1cb169d55c59fa99b80b33581a1727f12
Jarred Capellman <jcapellman@hotmail.com>
2019-03-11 21:41:54 +0000

    Added: Initial work on the BaseTable class

---
+++ src/jcIDS.web/DAL/Tables/Base/BaseTable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace jcIDS.web.DAL.Tables.Base
+{
+    public class BaseTable
+    {
+        public int ID { get; set; }
+
+        public DateTimeOffset Created { get; set; }
+
+        public DateTimeOffset Modified { get; set; }
+
+        public bool Active { get; set; }
+    }
+}
