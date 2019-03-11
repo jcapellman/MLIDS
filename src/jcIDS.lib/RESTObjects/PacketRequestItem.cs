403bc089ddd69b7b762e275c9a194b8ee1dc12e8
Jarred Capellman <jcapellman@hotmail.com>
2019-03-11 21:40:15 +0000

    Added: Initial work on the PacketRequestItem class

---
+++ src/jcIDS.lib/RESTObjects/PacketRequestItem.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+using jcIDS.lib.CommonObjects;
+using jcIDS.lib.RESTObjects.Base;
+
+namespace jcIDS.lib.RESTObjects
+{
+    public class PacketRequestItem : BaseRequestItem
+    {
+        public List<Packet> Packets { get; set; }
+    }
+}
