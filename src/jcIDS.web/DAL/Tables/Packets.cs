bf66da785e75edd84a2b27effa32c8c35e16de2f
Jarred Capellman <jcapellman@hotmail.com>
2019-03-11 21:37:24 +0000

    Added: Initial work on the Packets class

---
+++ src/jcIDS.web/DAL/Tables/Packets.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Sockets;
+
+using jcIDS.lib.CommonObjects;
+
+using jcIDS.web.DAL.Tables.Base;
+
+namespace jcIDS.web.DAL.Tables
+{
+    public class Packets : BaseTable
+    {
+        public string SourceIP { get; set; }
+
+        public string DestinationIP { get; set; }
+
+        public ProtocolType Protocol { get; set; }
+
+        public int DeviceID { get; set; }
+
+        public Packets(Packet packet)
+        {
+            SourceIP = packet.OriginationAddress;
+        }
+    }
+}
