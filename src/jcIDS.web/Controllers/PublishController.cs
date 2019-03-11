bdafa36a737a068ce181453f1ceb71e4e86577dd
Jarred Capellman <jcapellman@hotmail.com>
2019-03-11 17:09:30 +0000

    Added: Initial work on the REST Service

---
+++ src/jcIDS.web/Controllers/PublishController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+using jcIDS.lib.Objects;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace jcIDS.web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PublishController : ControllerBase
+    {
+        [HttpPost]
+        public void Post(List<PacketArrivedEventArgs> packet)
+        {
+
+        }
+    }
+}
