336f15f28a7dc014aeba1c32577a837da17d611e
Jarred Capellman <jcapellman@hotmail.com>
2019-03-11 21:37:03 +0000

    Added: Packet class

---
+++ src/jcIDS.lib/CommonObjects/Packet.cs
@@ -0,0 +1,45 @@
+﻿using System.Net.Sockets;
+
+namespace jcIDS.lib.CommonObjects
+{
+    public class Packet
+    {
+        public ProtocolType Protocol;
+
+        public string DestinationPort;
+
+        public string OriginationPort;
+
+        public string DestinationAddress;
+
+        public string OriginationAddress;
+
+        public string IPVersion;
+
+        public uint PacketLength;
+
+        public uint MessageLength;
+
+        public uint HeaderLength;
+
+        public byte[] ReceiveBuffer;
+
+        public byte[] IPHeaderBuffer;
+
+        public byte[] MessageBuffer;
+
+        public Packet()
+        {
+            Protocol = ProtocolType.Unspecified;
+
+            DestinationPort = "";
+            OriginationPort = "";
+            DestinationAddress = "";
+            OriginationAddress = "";
+            IPVersion = "";
+        }
+
+        public override string ToString() =>
+            $"{OriginationAddress}:{OriginationPort} ({Protocol}) - {DestinationAddress}:{DestinationPort} - {PacketLength}";
+    }
+}
