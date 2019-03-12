9b1098237266392f22e2308975621b0939c28bad
Jarred Capellman <jcapellman@hotmail.com>
2019-03-12 22:15:25 +0000

    Change: PublishController class now uses DI for the DBContext

--- src/jcIDS.web/Controllers/PublishController.cs
+++ src/jcIDS.web/Controllers/PublishController.cs
@@ -1,9 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 using jcIDS.lib.RESTObjects;
+
 using jcIDS.web.DAL;
 using jcIDS.web.DAL.Tables;
+using jcIDS.web.Managers;
 
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
@@ -12,32 +15,35 @@ namespace jcIDS.web.Controllers
 {
     public class PublishController : BaseApiController
     {
-        public PublishController(IMemoryCache memoryCache) : base(memoryCache) { }
+        public PublishController(IMemoryCache memoryCache, IDSContext dbContext) : base(memoryCache, dbContext) { }
 
-        private int GetDeviceIDFromToken(string deviceToken)
+        private int GetDeviceIdFromToken(string deviceToken)
         {
-            // TODO: Add actual logic
-            return default;
+            var deviceId = new DeviceManager(_cache, _dbContext).AuthenticateDevice(deviceToken);
+
+            if (deviceId == null)
+            {
+                throw new UnauthorizedAccessException($"{deviceToken} is invalid");
+            }
+
+            return deviceId.Value;
         }
 
         [HttpPost]
         public async Task Post(PacketRequestItem requestItem)
         {
-            var deviceID = GetDeviceIDFromToken(requestItem.DeviceToken);
+            var deviceID = GetDeviceIdFromToken(requestItem.DeviceToken);
 
-            using (var ef = new EFEntities())
+            var tasks = new List<Task>();
+
+            foreach (var packet in requestItem.Packets)
             {
-                var tasks = new List<Task>();
-
-                foreach (var packet in requestItem.Packets)
-                {
-                    tasks.Add(ef.Packets.AddAsync(new Packets(packet, deviceID)));
-                }
-
-                await Task.WhenAll(tasks);
-
-                await ef.SaveChangesAsync();
+                tasks.Add(_dbContext.Packets.AddAsync(new Packets(packet, deviceID)));
             }
+
+            await Task.WhenAll(tasks);
+
+            await _dbContext.SaveChangesAsync();
         }
     }
 }
