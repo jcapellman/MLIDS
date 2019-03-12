67934240e7a1fc2a694d432d0b2c03165826a68a
Jarred Capellman <jcapellman@hotmail.com>
2019-03-12 22:21:50 +0000

    Added: Initial work on the AuthController class

---
+++ src/jcIDS.web/Controllers/AuthController.cs
@@ -0,0 +1,23 @@
+﻿using jcIDS.web.DAL;
+using jcIDS.web.Managers;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace jcIDS.web.Controllers
+{
+    public class AuthController : BaseApiController
+    {
+        public AuthController(IMemoryCache memoryCache, IDSContext dbContext) : base(memoryCache, dbContext)
+        {
+        }
+
+        [HttpPost]
+        public string Post(string deviceName)
+        {
+            var device = new DeviceManager(Cache, DbContext).RegisterDevice(deviceName);
+
+            return device.Token;
+        }
+    }
+}
