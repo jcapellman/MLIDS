2d30bd083e4d77ee9505b40711c827e2fe8384da
Jarred Capellman <jcapellman@hotmail.com>
2019-03-12 21:43:18 +0000

    Added: Initial work on the Devices class

---
+++ src/jcIDS.web/DAL/Tables/Devices.cs
@@ -0,0 +1,11 @@
+﻿using jcIDS.web.DAL.Tables.Base;
+
+namespace jcIDS.web.DAL.Tables
+{
+    public class Devices : BaseTable
+    {
+        public string Name { get; set; }
+
+        public string Token { get; set; }
+    }
+}
