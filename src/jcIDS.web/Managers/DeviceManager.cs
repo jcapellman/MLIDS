5a5a819d4899593c6ce1765e2e09673521530f12
Jarred Capellman <jcapellman@hotmail.com>
2019-03-12 22:01:20 +0000

    Added: AuthenticateDevice method to the DeviceManager class

--- src/jcIDS.web/Managers/DeviceManager.cs
+++ src/jcIDS.web/Managers/DeviceManager.cs
@@ -1,18 +1,42 @@
 ﻿using System;
+using System.Linq;
 
 using jcIDS.lib.Helpers;
 using jcIDS.web.DAL;
 using jcIDS.web.DAL.Tables;
+
+using Microsoft.Extensions.Caching.Memory;
 
 namespace jcIDS.web.Managers
 {
     public class DeviceManager
     {
         private readonly EFEntities _efEntities;
+        private readonly IMemoryCache _memoryCache;
 
-        public DeviceManager(EFEntities eFEntities)
+        public DeviceManager(IMemoryCache memoryCache, EFEntities eFEntities)
         {
+            _memoryCache = memoryCache;
             _efEntities = eFEntities;
+        }
+
+        public int? AuthenticateDevice(string token)
+        {
+            if (_memoryCache.TryGetValue(token, out int id))
+            {
+                return id;
+            }
+
+            var device = _efEntities.Devices.FirstOrDefault(a => a.Token == token && a.Active);
+
+            if (device == null)
+            {
+                return null;
+            }
+
+            _memoryCache.Set(token, device.ID);
+
+            return device.ID;
         }
 
         public Devices RegisterDevice(string deviceName)
@@ -29,6 +53,8 @@ namespace jcIDS.web.Managers
             _efEntities.Devices.Add(device);
 
             _efEntities.SaveChanges();
+
+            _memoryCache.Set(device.Token, device.ID);
 
             return device;
         }
