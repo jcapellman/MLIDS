4d64b4474113f8610bb8aa7e21bfe505264c778d
Jarred Capellman <jcapellman@hotmail.com>
2019-03-12 21:49:26 +0000

    Added: Initial work on the DeviceManager class

---
+++ src/jcIDS.web/Managers/DeviceManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using jcIDS.lib.Helpers;
+using jcIDS.web.DAL;
+using jcIDS.web.DAL.Tables;
+
+namespace jcIDS.web.Managers
+{
+    public class DeviceManager
+    {
+        private readonly EFEntities _efEntities;
+
+        public DeviceManager(EFEntities eFEntities)
+        {
+            _efEntities = eFEntities;
+        }
+
+        public Devices RegisterDevice(string deviceName)
+        {
+            var device = new Devices
+            {
+                Active = true,
+                Created = DateTimeOffset.Now,
+                Modified = DateTimeOffset.Now,
+                Name = deviceName,
+                Token = deviceName.SHA1()
+            };
+
+            _efEntities.Devices.Add(device);
+
+            _efEntities.SaveChanges();
+
+            return device;
+        }
+    }
+}
