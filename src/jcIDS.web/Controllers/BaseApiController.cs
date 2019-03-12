9ac5b8ad38bb1c0e4abe861d45e8880fdd67f9b8
Jarred Capellman <jcapellman@hotmail.com>
2019-03-12 22:21:08 +0000

    Added: GetDeviceIdFromToken to the BaseApiController after moving it from the PublishController

--- src/jcIDS.web/Controllers/BaseApiController.cs
+++ src/jcIDS.web/Controllers/BaseApiController.cs
@@ -1,4 +1,7 @@
-﻿using jcIDS.web.DAL;
+﻿using System;
+
+using jcIDS.web.DAL;
+using jcIDS.web.Managers;
 
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
@@ -16,6 +19,18 @@ namespace jcIDS.web.Controllers
         {
             Cache = memoryCache;
             DbContext = dbContext;
+        }
+
+        protected int GetDeviceIdFromToken(string deviceToken)
+        {
+            var deviceId = new DeviceManager(Cache, DbContext).AuthenticateDevice(deviceToken);
+
+            if (deviceId == null)
+            {
+                throw new UnauthorizedAccessException($"{deviceToken} is invalid");
+            }
+
+            return deviceId.Value;
         }
     }
 }
--- src/jcIDS.web/Controllers/PublishController.cs
+++ src/jcIDS.web/Controllers/PublishController.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 
 using jcIDS.lib.RESTObjects;
 
 using jcIDS.web.DAL;
 using jcIDS.web.DAL.Tables;
-using jcIDS.web.Managers;
 
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
@@ -17,18 +15,6 @@ namespace jcIDS.web.Controllers
     {
         public PublishController(IMemoryCache memoryCache, IDSContext dbContext) : base(memoryCache, dbContext) { }
 
-        private int GetDeviceIdFromToken(string deviceToken)
-        {
-            var deviceId = new DeviceManager(_cache, _dbContext).AuthenticateDevice(deviceToken);
-
-            if (deviceId == null)
-            {
-                throw new UnauthorizedAccessException($"{deviceToken} is invalid");
-            }
-
-            return deviceId.Value;
-        }
-
         [HttpPost]
         public async Task Post(PacketRequestItem requestItem)
         {
@@ -38,12 +24,12 @@ namespace jcIDS.web.Controllers
 
             foreach (var packet in requestItem.Packets)
             {
-                tasks.Add(_dbContext.Packets.AddAsync(new Packets(packet, deviceID)));
+                tasks.Add(DbContext.Packets.AddAsync(new Packets(packet, deviceID)));
             }
 
             await Task.WhenAll(tasks);
 
-            await _dbContext.SaveChangesAsync();
+            await DbContext.SaveChangesAsync();
         }
     }
 }
