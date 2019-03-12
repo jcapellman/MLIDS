c456321123b0e8772904a6ae258f0777a08f9082
Jarred Capellman <jcapellman@hotmail.com>
2019-03-12 21:46:49 +0000

    Added: Initial work on the BaseApiController class

---
+++ src/jcIDS.web/Controllers/BaseApiController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace jcIDS.web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseApiController : ControllerBase
+    {
+
+    }
+}
