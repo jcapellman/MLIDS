b21e71403fda280522618549049c825d29f6ddce
Jarred Capellman <jcapellman@hotmail.com>
2019-03-12 21:58:22 +0000

    Added: MemoryCache to the ConfigureServices method of the Startup class

--- src/jcIDS.web/Startup.cs
+++ src/jcIDS.web/Startup.cs
@@ -17,6 +17,8 @@ namespace jcIDS.web
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMemoryCache();
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
