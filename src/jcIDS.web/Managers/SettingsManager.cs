60e4af43da495945080677490c37869c40b653e9
Jarred Capellman <jcapellman@hotmail.com>
2019-03-13 22:23:36 +0000

    Added: Initial work on the ParseConfiguration function

---
+++ src/jcIDS.web/Managers/SettingsManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+
+using jcIDS.web.Objects;
+
+using Microsoft.Extensions.Configuration;
+
+namespace jcIDS.web.Managers
+{
+    public class SettingsManager
+    {
+        public static ConfigurationValues ParseConfiguration(IConfiguration configuration)
+        {
+            var config = new ConfigurationValues();
+
+            var properties = typeof(ConfigurationValues).GetProperties().ToList();
+
+            foreach (var property in properties)
+            {
+                var propertyValue = configuration[property.Name];
+
+                property.SetValue(config, propertyValue);
+            }
+
+            return config;
+        }
+    }
+}
