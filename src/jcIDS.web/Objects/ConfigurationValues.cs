0c8e47ec8556618664055aae06c3f6647f4eaa00
Jarred Capellman <jcapellman@hotmail.com>
2019-03-13 22:24:00 +0000

    Added: Initial work on the ConfigurationValues class

---
+++ src/jcIDS.web/Objects/ConfigurationValues.cs
@@ -0,0 +1,9 @@
+﻿namespace jcIDS.web.Objects
+{
+    public class ConfigurationValues
+    {
+        public string DatabaseConnection { get; set; }
+
+        public bool AutoDeviceAdoption { get; set; }
+    }
+}
