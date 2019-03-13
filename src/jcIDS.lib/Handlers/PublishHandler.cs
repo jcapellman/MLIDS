09022b253b25757634980188c8f828d30bd3fbcc
Jarred Capellman <jcapellman@hotmail.com>
2019-03-13 21:25:02 +0000

    Added: Initial work on the PublishHandler

---
+++ src/jcIDS.lib/Handlers/PublishHandler.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+using jcIDS.lib.RESTObjects;
+
+namespace jcIDS.lib.Handlers
+{
+    public class PublishHandler : BaseHandler
+    {
+        public async Task<bool> SubmitPacketAsync(PacketRequestItem requestItem) =>
+            await PostAsync<PacketRequestItem, bool>("publish", requestItem);
+    }
+}
