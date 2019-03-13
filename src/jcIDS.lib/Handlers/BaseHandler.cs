d681ac46baff1f46c66d162d2ec4a4dcb68946c5
Jarred Capellman <jcapellman@hotmail.com>
2019-03-13 09:31:44 +0000

    Added: Initial work on the BaseHandler class

---
+++ src/jcIDS.lib/Handlers/BaseHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+namespace jcIDS.lib.Handlers
+{
+    public class BaseHandler
+    {
+        public async Task<TK> PostAsync<T, TK>(string url, T dataToPost)
+        {
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    var data = JsonConvert.SerializeObject(dataToPost);
+
+                    var result = await httpClient.PostAsync($"{Common.Constants.BASE_URL}{url}",
+                        new StringContent(data, Encoding.UTF8, "application/json"));
+
+                    var response = await result.Content.ReadAsStringAsync();
+
+                    return JsonConvert.DeserializeObject<TK>(response);
+                }
+            }
+            catch (Exception ex)
+            {
+                // TODO: Log exception
+
+                return default;
+            }
+        }
+    }
+}
