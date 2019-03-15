73b044e03abfc2efc8a202cc22d22bca6956f8eb
Jarred Capellman <jcapellman@hotmail.com>
2019-03-15 13:15:40 +0000

    Change: PublishHandler now implements the BaseHandler class, comment headers and uses the ReturnSet Container

--- src/jcIDS.lib/Handlers/PublishHandler.cs
+++ src/jcIDS.lib/Handlers/PublishHandler.cs
@@ -1,12 +1,24 @@
 ﻿using System.Threading.Tasks;
 
+using jcIDS.lib.CommonObjects;
 using jcIDS.lib.RESTObjects;
 
 namespace jcIDS.lib.Handlers
 {
     public class PublishHandler : BaseHandler
     {
-        public async Task<bool> SubmitPacketAsync(PacketRequestItem requestItem) =>
-            await PostAsync<PacketRequestItem, bool>("publish", requestItem);
+        public PublishHandler(string hostname) : base(hostname)
+        {
+        }
+
+        protected override string EndPoint => "publish";
+
+        /// <summary>
+        /// Submits Packets to the service
+        /// </summary>
+        /// <param name="requestItem">Packets to be submitted</param>
+        /// <returns>True if successfully submitted, False if in error</returns>
+        public async Task<ReturnSet<bool>> SubmitPacketAsync(PacketRequestItem requestItem) =>
+            await PostAsync<PacketRequestItem, bool>(EndPoint, requestItem);
     }
 }
