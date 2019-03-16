a0e85ec821bc8428dfc8efd0a181a4427ea2c6f8
Jarred Capellman <jcapellman@hotmail.com>
2019-03-16 09:43:02 -0400

    Added: Initial work on the Prediction class

---
+++ src/jcIDS.ML.lib/Prediction.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+
+using jcIDS.lib.CommonObjects;
+using jcIDS.ML.lib.Objects;
+
+using Microsoft.ML;
+
+namespace jcIDS.ML.lib
+{
+    public class Prediction
+    {
+        private MLContext _mlContext;
+
+        public Prediction()
+        {
+            _mlContext = new MLContext();
+        }
+
+        public bool Predict(byte[] model, Packet packet)
+        {
+            ITransformer trainedModel;
+
+            using (var ms = new MemoryStream(model))
+            {
+                trainedModel = _mlContext.Model.Load(ms);
+            }
+
+            var predFunction = trainedModel.CreatePredictionEngine<Packet, PredictionResult>(_mlContext);
+
+            var prediction = predFunction.Predict(packet);
+
+            return prediction.Malicious;
+        }
+    }
+}
