0a811c41578e026ff172bf90146ff9119edb681a
Jarred Capellman <jcapellman@hotmail.com>
2019-03-16 01:30:34 -0400

    Added: SaveChanges override to properly set all of the Base Fields on Save

--- src/jcIDS.web/DAL/IDSContext.cs
+++ src/jcIDS.web/DAL/IDSContext.cs
@@ -1,4 +1,8 @@
-﻿using jcIDS.web.DAL.Tables;
+﻿using System;
+using System.Linq;
+
+using jcIDS.web.DAL.Tables;
+using jcIDS.web.DAL.Tables.Base;
 
 using Microsoft.EntityFrameworkCore;
 
@@ -11,6 +15,34 @@ namespace jcIDS.web.DAL
         public DbSet<Packets> Packets { get; set; }
 
         public IDSContext(DbContextOptions options) : base(options) { }
+
+        public override int SaveChanges()
+        {
+            var changeSet = ChangeTracker.Entries<BaseTable>();
+
+            if (changeSet == null)
+            {
+                return base.SaveChanges();
+            }
+
+            foreach (var entry in changeSet.Where(c => c.State != EntityState.Unchanged))
+            {
+                entry.Entity.Modified = DateTimeOffset.Now;
+
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.Created = DateTimeOffset.Now;
+                        entry.Entity.Active = true;
+                        break;
+                    case EntityState.Deleted:
+                        entry.Entity.Active = false;
+                        break;
+                }
+            }
+
+            return base.SaveChanges();
+        }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
