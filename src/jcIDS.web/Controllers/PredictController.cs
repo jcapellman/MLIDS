6bedd97376c70eb752572ba4b56b79c47300e8aa
Jarred Capellman <jcapellman@hotmail.com>
2019-03-16 09:43:28 -0400

    Added: Initial work on the PredictController class

---
+++ src/jcIDS.web/Controllers/PredictController.cs
@@ -0,0 +1,32 @@
+﻿using jcIDS.web.DAL;
+using jcIDS.web.Managers;
+using jcIDS.web.Objects;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace jcIDS.web.Controllers
+{
+    public class PredictController : BaseApiController
+    {
+        public PredictController(IMemoryCache memoryCache, IDSContext dbContext, ConfigurationValues configuration) : base(memoryCache, dbContext, configuration)
+        {
+        }
+
+        [HttpGet]
+        public byte[] GetModel(int deviceId)
+        {
+            var result = new DeviceManager(Cache, DbContext).GetDeviceModel(deviceId);
+
+            if (!result.HasObjectError)
+            {
+                return result.ObjectValue;
+            }
+
+            Log.Error(result.ObjectException, result.ObjectExceptionInformation);
+
+            return null;
+
+        }
+    }
+}
