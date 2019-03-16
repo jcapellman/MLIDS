9d508350d3b5f2669912246bd1cf62d58375f1d8
Jarred Capellman <jcapellman@hotmail.com>
2019-03-16 00:50:25 -0400

    Added: DefaultValue for the AutoDeviceAdoption property in the ConfigurationValues class

--- src/jcIDS.web/Objects/ConfigurationValues.cs
+++ src/jcIDS.web/Objects/ConfigurationValues.cs
@@ -1,9 +1,12 @@
-﻿namespace jcIDS.web.Objects
+﻿using System.ComponentModel;
+
+namespace jcIDS.web.Objects
 {
     public class ConfigurationValues
     {
         public string DatabaseConnection { get; set; }
 
+        [DefaultValue(true)]
         public bool AutoDeviceAdoption { get; set; }
     }
 }
