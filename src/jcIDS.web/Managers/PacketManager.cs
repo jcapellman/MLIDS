62c6b0bdd755364c2a23457fde4bd207ddce3c2b
Jarred Capellman <jcapellman@hotmail.com>
2019-03-16 01:36:32 -0400

    Added: Initial work on the PacketManager class

---
+++ src/jcIDS.web/Managers/PacketManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using jcIDS.lib.CommonObjects;
+using jcIDS.web.DAL;
+using jcIDS.web.DAL.Tables;
+
+namespace jcIDS.web.Managers
+{
+    public class PacketManager
+    {
+        private readonly IDSContext _dbContext;
+
+        public PacketManager(IDSContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<bool> StorePacketsAsync(List<Packet> packets, int deviceId)
+        {
+            var tasks = new List<Task>();
+
+            foreach (var packet in packets)
+            {
+                tasks.Add(_dbContext.Packets.AddAsync(new Packets(packet, deviceId)));
+            }
+
+            await Task.WhenAll(tasks);
+
+            var result = await _dbContext.SaveChangesAsync();
+
+            return result > 0;
+        }
+    }
+}
