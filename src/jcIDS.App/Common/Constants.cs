c17dd5c5cc31449f7c6362e314c21efcd24585e2
Jarred Capellman <jcapellman@hotmail.com>
2019-04-16 21:20:31 -0500

    Added: DEFAULT_WEBSERVICE_URL constant in the Constants class

--- src/jcIDS.App/Common/Constants.cs
+++ src/jcIDS.App/Common/Constants.cs
@@ -3,5 +3,7 @@
     public class Constants
     {
         public const string DEFAULT_SETTINGS_FILE = "settings.json";
+
+        public const string DEFAULT_WEBSERVICE_URL = "http://localhost:8885/";
     }
 }
