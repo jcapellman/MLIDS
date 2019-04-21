66bdcb1bc007bd3521a103d39ec0c9e0daffefd1
Jarred Capellman <jcapellman@hotmail.com>
2019-04-20 21:38:22 -0500

    Change: Replaced the Program class with the new .NET Core Service

--- src/jcIDS.App/Program.cs
+++ src/jcIDS.App/Program.cs
@@ -1,102 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
-    
-using jcIDS.app.Common;
-using jcIDS.app.Managers;
-using jcIDS.lib.CommonObjects;
-using jcIDS.lib.Enums;
-using jcIDS.lib.Handlers;
-using jcIDS.lib.Managers;
-using jcIDS.lib.RESTObjects;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace jcIDS.app
 {
-    class Program
+    public class Program
     {
-        private static string DeviceToken;
-
-        private static async Task<bool> AuthenticateAsync(string hostName, string webServiceURL)
+        public static void Main(string[] args)
         {
-            using (var authHandler = new AuthHandler(webServiceURL))
-            {
-                var result = await authHandler.RegisterDeviceAsync(hostName);
-
-                if (!result.HasObjectError)
-                {
-                    return true;
-                }
-
-                DeviceToken = result.ObjectValue;
-
-                Console.WriteLine($"Error occurred registering: {result.ObjectException} | {result.ObjectExceptionInformation}");
-
-                return false;
-            }
+            CreateHostBuilder(args).Build().Run();
         }
 
-        static async Task Main(string[] args)
-        {
-            var settings = SettingsManager.LoadSettings();
-
-            if (settings == null)
-            {
-                settings = new Settings();
-
-                Console.WriteLine("Settings not set, using defaults...");
-            }
-
-            Console.WriteLine($"Using {settings.WebServiceURL} to authenticate...");
-
-            var authResult = await AuthenticateAsync(Environment.MachineName, settings.WebServiceURL);
-
-            if (!authResult)
-            {
-                Console.WriteLine("Quitting due to registration failure");
-
-                return;
-            }
-
-            Console.WriteLine("App is authenticated, starting socket listener...");
-
-            using (var sListener = new SocketListener())
-            {
-                sListener.PacketArrival += PacketArrival;
-
-                var runResult = sListener.Run();
-
-                if (runResult.ObjectValue != SocketCreationStatusCode.OK)
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(loggerFactory => loggerFactory.AddEventSourceLogger())
+                .UseServiceBaseLifetime()
+                .ConfigureServices(services =>
                 {
-                    switch (runResult.ObjectValue)
-                    {
-                        case SocketCreationStatusCode.ACCESS_DENIED:
-                            Console.WriteLine("Access Denied, are you running as Administrator/root?");
-                            break;
-                        case SocketCreationStatusCode.UNKNOWN:
-                            Console.WriteLine(runResult.HasObjectError
-                                ? $"Run Error: {runResult.ObjectException}"
-                                : "Unknown Error Occurred");
-                            break;
-                    }
-                }
-
-                while (true) { Thread.Sleep(200); }
-            }
-        }
-
-        private static async void PacketArrival(object sender, Packet packet)
-        {
-            using (var pHandler = new PublishHandler(Environment.MachineName))
-            {
-                await pHandler.SubmitPacketAsync(new PacketRequestItem
-                {
-                    Packets = new List<Packet> {packet},
-                    DeviceToken = DeviceToken
+                    services.AddHostedService<Worker>();
                 });
-
-                Console.WriteLine(packet);
-            }
-        }
     }
 }
