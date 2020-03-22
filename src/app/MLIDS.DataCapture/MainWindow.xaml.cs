619ff5ad7fa42a53bf40ce00e947259906ed491f
Jarred Capellman <jcapellman@hotmail.com>
2020-03-22 13:47:43 -0500

    Added: Initial GUI on the NpCap

---
+++ src/app/MLIDS.DataCapture/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using MLIDS.DataCapture.ViewModels;
+
+using System.Windows;
+
+namespace MLIDS.DataCapture
+{
+    public partial class MainWindow : Window
+    {
+        private MainViewModel VM => (MainViewModel)DataContext;
+
+        public MainWindow()
+        {
+            InitializeComponent();                        
+        }
+
+        private void btnStartCapturing_Click(object sender, RoutedEventArgs e)
+        {
+            VM.StartCapture();
+        }
+
+        private void btnStopCapturing_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ src/app/MLIDS.DataCapture/ViewModels/MainViewModel.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+
+using SharpPcap;
+using SharpPcap.Npcap;
+
+namespace MLIDS.DataCapture.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private List<ICaptureDevice> _deviceList = new List<ICaptureDevice>();
+
+        public List<ICaptureDevice> DeviceList
+        {
+            get => _deviceList;
+
+            set
+            {
+                _deviceList = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        private ICaptureDevice _selectedDevice;
+
+        public ICaptureDevice SelectedDevice
+        {
+            get => _selectedDevice;
+
+            set
+            {
+                _selectedDevice = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        private ObservableCollection<string> _packets = new ObservableCollection<string>();
+
+        public ObservableCollection<string> Packets
+        {
+            get => _packets;
+
+            set
+            {
+                _packets = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            DeviceList = CaptureDeviceList.Instance.ToList();
+
+            SelectedDevice = DeviceList.FirstOrDefault();
+        }
+
+        public void StartCapture()
+        {
+            if (SelectedDevice is NpcapDevice)
+            {
+                var nPcap = SelectedDevice as NpcapDevice;
+
+                nPcap.Open(SharpPcap.Npcap.OpenFlags.DataTransferUdp | SharpPcap.Npcap.OpenFlags.NoCaptureLocal, 1000);
+            }
+
+            SelectedDevice.OnPacketArrival += new PacketArrivalEventHandler(device_OnPacketArrival);
+            SelectedDevice.StartCapture();
+        }
+
+        private void device_OnPacketArrival(object sender, CaptureEventArgs e)
+        {
+            System.Windows.Application.Current.Dispatcher.Invoke((Action)delegate
+            {
+                Packets.Add(e.Packet.ToString());
+            });
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
