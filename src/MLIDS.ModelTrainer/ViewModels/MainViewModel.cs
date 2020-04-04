0ed9cfd19ad629f2b2fd7051f5ba652775320685
Jarred Capellman <jcapellman@hotmail.com>
2020-04-04 10:17:01 -0500

    Added: Initial work on the Model Trainer

---
+++ src/MLIDS.ModelTrainer/ViewModels/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace MLIDS.ModelTrainer.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
