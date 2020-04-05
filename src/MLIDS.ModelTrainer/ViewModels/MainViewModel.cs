bb0daa35aacd7b210e9d3405ec0a3b5152a0f44d
Jarred Capellman <jcapellman@hotmail.com>
2020-04-05 16:12:48 -0500

    Added: Property Mapping to User Input Fields

--- src/MLIDS.ModelTrainer/ViewModels/MainViewModel.cs
+++ src/MLIDS.ModelTrainer/ViewModels/MainViewModel.cs
@@ -4,6 +4,83 @@ namespace MLIDS.ModelTrainer.ViewModels
 {
     public class MainViewModel : INotifyPropertyChanged
     {
+        private string _locationCleanTrafficFile;
+
+        public string LocationCleanTrafficFile
+        {
+            get => _locationCleanTrafficFile;
+
+            set
+            {
+                _locationCleanTrafficFile = value;
+
+                OnPropertyChanged();
+
+                UpdateTrainButton();
+            }
+        }
+
+        private string _locationMaliciousTrafficFile;
+
+        public string LocationMaliciousTrafficFile
+        {
+            get => _locationMaliciousTrafficFile;
+
+            set
+            {
+                _locationMaliciousTrafficFile = value;
+
+                OnPropertyChanged();
+
+                UpdateTrainButton();
+            }
+        }
+
+        private string _locationModelFile;
+
+        public string LocationModelFile
+        {
+            get => _locationModelFile;
+
+            set
+            {
+                _locationModelFile = value;
+
+                OnPropertyChanged();
+
+                UpdateTrainButton();
+            }
+        }
+
+        private bool _btnTrainEnable;
+
+        public bool btnTrainEnable
+        {
+            get => _btnTrainEnable;
+
+            set
+            {
+                _btnTrainEnable = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _isTraining = false;
+
+        private void UpdateTrainButton()
+        {
+            btnTrainEnable = !string.IsNullOrEmpty(LocationCleanTrafficFile) &&
+                             !string.IsNullOrEmpty(LocationMaliciousTrafficFile) &&
+                             !string.IsNullOrEmpty(LocationModelFile) &&
+                             !_isTraining;
+        }
+
+        public MainViewModel()
+        {
+            UpdateTrainButton();
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         protected virtual void OnPropertyChanged(string propertyName = null)
