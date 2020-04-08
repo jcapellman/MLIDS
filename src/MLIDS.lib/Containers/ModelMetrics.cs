68933e563d84e4d9d7199da9c413a0a7a2c09c34
Jarred Capellman <jcapellman@hotmail.com>
2020-04-08 18:48:52 -0500

    Added: ModelMetrics class

---
+++ src/MLIDS.lib/Containers/ModelMetrics.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using Microsoft.ML.Data;
+
+namespace MLIDS.lib.Containers
+{
+    public class ModelMetrics
+    {
+        public TimeSpan Duration { get; set; }
+
+        public AnomalyDetectionMetrics Metrics { get; set; }
+
+        public int NumCleanRows { get; set; }
+
+        public int NumMaliciousRows { get; set; }
+    }
+}
