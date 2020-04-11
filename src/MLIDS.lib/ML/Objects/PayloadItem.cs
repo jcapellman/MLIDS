052237c553778e84131ba9b6a1964cd5b2edaaee
Jarred Capellman <jcapellman@hotmail.com>
2020-04-11 16:06:21 -0500

    Added: Label property in the PayloadItem class

--- src/MLIDS.lib/ML/Objects/PayloadItem.cs
+++ src/MLIDS.lib/ML/Objects/PayloadItem.cs
@@ -9,31 +9,36 @@ namespace MLIDS.lib.ML.Objects
     public class PayloadItem
     {
         [LoadColumn(0)]
-        public string ProtocolType { get; private set; }
+        public bool Label { get; private set; }
 
         [LoadColumn(1)]
-        public string SourceIPAddress { get; private set; }
+        public string ProtocolType { get; private set; }
 
         [LoadColumn(2)]
-        public int SourcePort { get; private set; }
+        public string SourceIPAddress { get; private set; }
 
         [LoadColumn(3)]
-        public string DestinationIPAddress { get; private set; }
+        public int SourcePort { get; private set; }
 
         [LoadColumn(4)]
-        public int DestinationPort { get; private set; }
+        public string DestinationIPAddress { get; private set; }
 
         [LoadColumn(5)]
-        public int HeaderSize { get; private set; }
+        public int DestinationPort { get; private set; }
 
         [LoadColumn(6)]
-        public int PayloadSize { get; private set; }
+        public int HeaderSize { get; private set; }
 
         [LoadColumn(7)]
+        public int PayloadSize { get; private set; }
+
+        [LoadColumn(8)]
         public string PacketContent { get; private set; }
 
-        public PayloadItem(string protocolType, IPPacket sourcePacket, TransportPacket payloadPacket)
+        public PayloadItem(string protocolType, IPPacket sourcePacket, TransportPacket payloadPacket, bool clean)
         {
+            Label = clean;
+
             ProtocolType = protocolType;
 
             SourceIPAddress = sourcePacket.SourceAddress.ToString();
