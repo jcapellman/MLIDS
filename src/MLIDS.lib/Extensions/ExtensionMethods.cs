ef2303f52bfd1d200517c27ebba48b5cf4939825
Jarred Capellman <jcapellman@hotmail.com>
2020-04-11 16:04:51 -0500

    Added: Initial work on the ExtensionMethods class

---
+++ src/MLIDS.lib/Extensions/ExtensionMethods.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+
+namespace MLIDS.lib.Extensions
+{
+    public static class ExtensionMethods
+    {
+        public static string[] ToPropertyList<T>(this Type objType, string labelName) => 
+            objType.GetProperties().Where(a => a.Name != labelName).Select(a => a.Name).ToArray();
+    }
+}
