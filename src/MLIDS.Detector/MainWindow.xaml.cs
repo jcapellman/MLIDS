20ce9d848cd55833cd30e02369eb546caa60f9ad
Jarred Capellman <jcapellman@hotmail.com>
2020-04-19 21:09:13 -0500

    Added: Initial work on the Detector Window

--- src/MLIDS.Detector/MainWindow.xaml.cs
+++ src/MLIDS.Detector/MainWindow.xaml.cs
@@ -1,12 +1,26 @@
 ﻿using System.Windows;
 
+using MLIDS.Detector.ViewModels;
+
 namespace MLIDS.Detector
 {
     public partial class MainWindow : Window
     {
+        private MainViewModel Vm => (MainViewModel)DataContext;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnStartCapturing_Click(object sender, RoutedEventArgs e)
+        {
+            Vm.StartCapture();
+        }
+
+        private void btnStopCapturing_Click(object sender, RoutedEventArgs e)
+        {
+            Vm.StopCapture();
         }
     }
 }
