9a9f1f7b1a27a80a11c66052259bb2c2cea2cdd7
Jarred Capellman <jcapellman@hotmail.com>
2020-04-25 13:22:32 -0500

    Added: Common code into the MVVM

---
+++ src/MLIDS.lib/ViewModels/BaseViewModel.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+using SharpPcap;
+
+namespace MLIDS.lib.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        private bool _startBtnEnabled;
+
+        public bool StartBtnEnabled
+        {
+            get => _startBtnEnabled;
+
+            set
+            {
+                _startBtnEnabled = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _deviceSelectionEnabled;
+
+        public bool DeviceSelectionEnabled
+        {
+            get => _deviceSelectionEnabled;
+
+            set
+            {
+                _deviceSelectionEnabled = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _stopBtnEnabled;
+
+        public bool StopBtnEnabled
+        {
+            get => _stopBtnEnabled;
+
+            set
+            {
+                _stopBtnEnabled = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        private List<ICaptureDevice> _deviceList = new List<ICaptureDevice>();
+
+        public List<ICaptureDevice> DeviceList
+        {
+            get => _deviceList;
+
+            set
+            {
+                _deviceList = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        private ICaptureDevice _selectedDevice;
+
+        public ICaptureDevice SelectedDevice
+        {
+            get => _selectedDevice;
+
+            set
+            {
+                _selectedDevice = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _isRunning;
+
+        public bool IsRunning
+        {
+            get => _isRunning;
+
+            set
+            {
+                _isRunning = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        private ObservableCollection<string> _packets = new ObservableCollection<string>();
+
+        public ObservableCollection<string> Packets
+        {
+            get => _packets;
+
+            set
+            {
+                _packets = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        public BaseViewModel()
+        {
+            DeviceList = CaptureDeviceList.Instance.ToList();
+
+            SelectedDevice = DeviceList.FirstOrDefault();
+
+            StartBtnEnabled = false;
+            StopBtnEnabled = false;
+            DeviceSelectionEnabled = true;
+            IsRunning = false;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
