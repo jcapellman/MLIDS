5a5a6e04d51d2d19d5e0cf7d7f7da7fecc866fa0
Jarred Capellman <jcapellman@hotmail.com>
2020-04-26 08:40:34 -0500

    Change: Updated the DataCapture MainWindow to use the BaseWindow Class

--- src/MLIDS.DataCapture/MainWindow.xaml.cs
+++ src/MLIDS.DataCapture/MainWindow.xaml.cs
@@ -1,26 +1,12 @@
-﻿using System.Windows;
-
-using MLIDS.DataCapture.ViewModels;
+﻿using MLIDS.lib.Windows;
 
 namespace MLIDS.DataCapture
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : BaseWindow
     {
-        private MainViewModel Vm => (MainViewModel)DataContext;
-
         public MainWindow()
         {
             InitializeComponent();                        
-        }
-
-        private void btnStartCapturing_Click(object sender, RoutedEventArgs e)
-        {
-            Vm.StartCapture();
-        }
-
-        private void btnStopCapturing_Click(object sender, RoutedEventArgs e)
-        {
-            Vm.StopCapture();
         }
     }
 }
