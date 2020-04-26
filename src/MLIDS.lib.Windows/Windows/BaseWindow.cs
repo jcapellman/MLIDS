156e6c39f7b6614e14e6c794f341875f47d51acf
Jarred Capellman <jcapellman@hotmail.com>
2020-04-26 08:48:59 -0500

    Added: Initial work on the BaseWindow class

---
+++ src/MLIDS.lib.Windows/Windows/BaseWindow.cs
@@ -0,0 +1,34 @@
+﻿using MLIDS.lib.ViewModels;
+
+using System.Linq;
+using System.Windows;
+
+namespace MLIDS.lib.Windows
+{
+    public class BaseWindow : Window
+    {
+        public BaseViewModel Vm => (BaseViewModel)DataContext;
+
+        public void SanityCheckDriver()
+        {
+            if (Vm.DeviceList.Any())
+            {
+                return;
+            }
+
+            MessageBox.Show("NPCAP Driver not installed - please install (https://nmap.org/npcap/)");
+
+            Application.Current.Shutdown();
+        }
+
+        public void btnStartAction_Click(object sender, RoutedEventArgs e)
+        {
+            Vm.StartAction();
+        }
+
+        public void btnStopAction_Click(object sender, RoutedEventArgs e)
+        {
+            Vm.StopAction();
+        }
+    }
+}
