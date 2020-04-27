f397fa47194aaae9043eb0949b11ce8b194b6dee
Jarred Capellman <jcapellman@hotmail.com>
2020-04-27 17:55:46 -0500

    Added: Initial work on the Constants class

---
+++ src/MLIDS.lib/Common/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace MLIDS.lib.Common
+{
+    public static class Constants
+    {
+        public const string DAL_HostIP = "127.0.0.1";
+
+        public const int DAL_HostPort = 27017;
+
+        public const int API_VERSION = 1;
+    }
+}
