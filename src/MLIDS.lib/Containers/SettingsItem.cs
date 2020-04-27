f3c6dca1106bbe319c86816ae19a769f8a21a2d8
Jarred Capellman <jcapellman@hotmail.com>
2020-04-27 17:53:49 -0500

    Change: SettingsItem Class now is set to the constant values by default

--- src/MLIDS.lib/Containers/SettingsItem.cs
+++ src/MLIDS.lib/Containers/SettingsItem.cs
@@ -1,9 +1,18 @@
-﻿namespace MLIDS.lib.Containers
+﻿using MLIDS.lib.Common;
+
+namespace MLIDS.lib.Containers
 {
     public class SettingsItem
     {
         public string DAL_HostIP { get; set; }
 
         public int DAL_HostPort { get; set; }
+
+        public SettingsItem()
+        {
+            DAL_HostIP = Constants.DAL_HostIP;
+
+            DAL_HostPort = Constants.DAL_HostPort;
+        }
     }
 }
