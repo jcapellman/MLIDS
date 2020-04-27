81c1bbf08c2ec261848109fe50ed360bb0c56544
Jarred Capellman <jcapellman@hotmail.com>
2020-04-27 14:32:05 -0500

    Added: Initial work on the BaseDAL Class

---
+++ src/MLIDS.lib/DAL/Base/BaseDAL.cs
@@ -0,0 +1,17 @@
+﻿using MLIDS.lib.ML.Objects;
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MLIDS.lib.DAL.Base
+{
+    public abstract class BaseDAL
+    {
+        public abstract Task<bool> WritePacketAsync(PayloadItem packet);
+
+        public abstract Task<List<PayloadItem>> GetHostPacketsAsync(string hostName);
+
+        public abstract Task<List<PayloadItem>> QueryPacketsAsync(System.Linq.Expressions.Expression<Func<PayloadItem, bool>> queryExpression);
+    }
+}
