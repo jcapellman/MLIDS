970b4822870355652b0c0e0423dcbe9b1d207aa3
Jarred Capellman <jcapellman@hotmail.com>
2020-04-27 19:21:22 -0500

    Fix: Filtering out NoColumn properties from the ToPropertyList Extension Method

--- src/MLIDS.lib/Extensions/ExtensionMethods.cs
+++ src/MLIDS.lib/Extensions/ExtensionMethods.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Linq;
 
+using Microsoft.ML.Data;
+
 namespace MLIDS.lib.Extensions
 {
     public static class ExtensionMethods
     {
         public static string[] ToPropertyList<T>(this Type objType, string labelName) => 
-            objType.GetProperties().Where(a => a.Name != labelName).Select(a => a.Name).ToArray();
+            objType.GetProperties().Where(a => a.Name != labelName && 
+                a.CustomAttributes.All(b => b.AttributeType != typeof(NoColumnAttribute)))
+                .Select(a => a.Name).ToArray();
     }
 }
