d54b38a6eb50ad9a88794a6c382d6d5494723033
Jarred Capellman <jcapellman@hotmail.com>
2020-04-30 19:16:57 -0500

    Change: Removed consolidated duplicate code from the MainViewModel class in the Data Capture application

--- src/MLIDS.DataCapture/ViewModels/MainViewModel.cs
+++ src/MLIDS.DataCapture/ViewModels/MainViewModel.cs
@@ -1,15 +1,12 @@
-﻿using System;
-
-using MLIDS.lib.Windows.ViewModels;
+﻿using MLIDS.lib.Windows.ViewModels;
 
 using PacketDotNet;
 
 using SharpPcap;
-using SharpPcap.Npcap;
 
 namespace MLIDS.DataCapture.ViewModels
 {
-    public class MainViewModel : BaseViewModel
+    public class MainViewModel : BaseCaptureMainViewModel
     {
         private bool _enableSaveStream;
 
@@ -39,62 +36,12 @@ namespace MLIDS.DataCapture.ViewModels
             }
         }
 
-        private bool _chkBxSaveEnabled;
-
-        public bool ChkBxSaveEnabled
-        {
-            get => _chkBxSaveEnabled;
-
-            set
-            {
-                _chkBxSaveEnabled = value;
-
-                OnPropertyChanged();
-            }
-        }
-
         public MainViewModel()
         {
-            ChkBxSaveEnabled = true;
-
             StartBtnEnabled = true;
         }
 
-        public override void StartAction()
-        {
-            StartBtnEnabled = false;
-            StopBtnEnabled = true;
-            DeviceSelectionEnabled = false;
-            ChkBxSaveEnabled = false;
-
-            if (SelectedDevice is NpcapDevice)
-            {
-                var nPcap = SelectedDevice as NpcapDevice;
-
-                nPcap?.Open(OpenFlags.DataTransferUdp | OpenFlags.NoCaptureLocal, 1000);
-            }
-
-            SelectedDevice.OnPacketArrival += Device_OnPacketArrival;
-            SelectedDevice.StartCapture();
-        }
-
-        public override void StopAction()
-        {
-            try
-            {
-                SelectedDevice.OnPacketArrival -= Device_OnPacketArrival;
-
-                SelectedDevice.StopCapture();
-                SelectedDevice.Close();
-            } catch (Exception) { }
-
-            StartBtnEnabled = true;
-            StopBtnEnabled = false;
-            DeviceSelectionEnabled = true;
-            ChkBxSaveEnabled = true;
-        }
-
-        private void Device_OnPacketArrival(object sender, CaptureEventArgs e)
+        public override void PacketProcessing(CaptureEventArgs e)
         {
             System.Windows.Application.Current.Dispatcher.Invoke(delegate
             {
@@ -115,7 +62,8 @@ namespace MLIDS.DataCapture.ViewModels
                 if (EnableSaveStream)
                 {
                     _dataStorage.WritePacketAsync(payloadItem);
-                } else
+                }
+                else
                 {
                     Packets.Add(payloadItem.ToString());
                 }
