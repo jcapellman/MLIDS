772eb093b8ea51ef8539ae66b00e6dcee27e5c5d
Jarred Capellman <jcapellman@hotmail.com>
2020-04-30 19:16:14 -0500

    Added: BaseCaptureMainViewModel abstract class to consolidate code

---
+++ src/MLIDS.lib.Windows/ViewModels/BaseCaptureViewModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+using SharpPcap;
+using SharpPcap.Npcap;
+
+namespace MLIDS.lib.Windows.ViewModels
+{
+    public abstract class BaseCaptureMainViewModel : BaseViewModel
+    {
+        private bool _chkBxSaveEnabled;
+
+        public bool ChkBxSaveEnabled
+        {
+            get => _chkBxSaveEnabled;
+
+            set
+            {
+                _chkBxSaveEnabled = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        public BaseCaptureMainViewModel()
+        {
+            ChkBxSaveEnabled = true;
+        }
+
+        public override void StartAction()
+        {
+            StartBtnEnabled = false;
+            StopBtnEnabled = true;
+            DeviceSelectionEnabled = false;
+            ChkBxSaveEnabled = false;
+
+            if (SelectedDevice is NpcapDevice)
+            {
+                var nPcap = SelectedDevice as NpcapDevice;
+
+                nPcap?.Open(OpenFlags.DataTransferUdp | OpenFlags.NoCaptureLocal, 1000);
+            }
+
+            SelectedDevice.OnPacketArrival += Device_OnPacketArrival;
+            SelectedDevice.StartCapture();
+        }
+
+        public override void StopAction()
+        {
+            try
+            {
+                SelectedDevice.OnPacketArrival -= Device_OnPacketArrival;
+
+                SelectedDevice.StopCapture();
+                SelectedDevice.Close();
+            }
+            catch (Exception) { }
+
+            StartBtnEnabled = true;
+            StopBtnEnabled = false;
+            DeviceSelectionEnabled = true;
+            ChkBxSaveEnabled = true;
+        }
+
+        public abstract void PacketProcessing(CaptureEventArgs e);
+
+        private void Device_OnPacketArrival(object sender, CaptureEventArgs e)
+        {
+            PacketProcessing(e);
+        }
+    }
+}
