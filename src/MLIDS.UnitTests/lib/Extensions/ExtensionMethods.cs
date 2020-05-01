bc082d492a364e4a785eb46b660ebea63759256b
Jarred Capellman <jcapellman@hotmail.com>
2020-05-01 17:36:47 -0500

    Added: ExtensionMethods and CSV Coverage #18 #19

---
+++ src/MLIDS.UnitTests/lib/Extensions/ExtensionMethods.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using MLIDS.lib.Containers;
+using MLIDS.lib.Extensions;
+
+namespace MLIDS.UnitTests.lib.Extensions
+{
+    [TestClass]
+    public class ExtensionMethods
+    {
+        [TestMethod]
+        public void ExtensionMethods_ClassTestNullLabel()
+        {
+            typeof(ModelMetrics).ToPropertyList<ModelMetrics>(null);
+        }
+
+        [TestMethod]
+        public void ExtensionMethods_ClassTestActualLabel()
+        {
+            typeof(ModelMetrics).ToPropertyList<ModelMetrics>("test");
+        }
+
+        [TestMethod]
+        public void ExtensionMethods_StringTest()
+        {
+            typeof(string).ToPropertyList<ModelMetrics>(null);
+        }
+    }
+}
