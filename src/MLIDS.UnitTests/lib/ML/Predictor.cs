5a549bb17abb9ca4405c3209172ea9d3eba1e4e8
Jarred Capellman <jcapellman@hotmail.com>
2020-05-02 14:16:09 -0500

    Added: Initial work on the Predictor class unit tests #22

---
+++ src/MLIDS.UnitTests/lib/ML/Predictor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MLIDS.UnitTests.lib.ML
+{
+    [TestClass]
+    public class Predictor
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Predictor_NullTest()
+        {
+            new MLIDS.lib.ML.Predictor(null);
+        }
+    }
+}
--- src/MLIDS.lib/ML/Predictor.cs
+++ src/MLIDS.lib/ML/Predictor.cs
@@ -2,14 +2,22 @@
 
 using MLIDS.lib.ML.Objects;
 
+using System;
+using System.Diagnostics.CodeAnalysis;
+
 namespace MLIDS.lib.ML
 {
     public class Predictor
     {
         private readonly PredictionEngine<PayloadItem, PredictionItem> _predictionEngine;
 
-        public Predictor(string modelName)
+        public Predictor([NotNull]string modelName)
         {
+            if (string.IsNullOrEmpty(modelName))
+            {
+                throw new ArgumentNullException(nameof(modelName));
+            }
+
             var mlContext = new MLContext(2020);
 
             var model = mlContext.Model.Load(modelName, out _);
