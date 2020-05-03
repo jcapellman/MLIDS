c88afbf5668b235f07dbf7c795ff255d7c3d9083
Jarred Capellman <jcapellman@hotmail.com>
2020-05-03 15:27:46 -0500

    Added: Initial Unit tests for the Model Trainer #14

---
+++ src/MLIDS.UnitTests/ModelTrainer/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MLIDS.UnitTests.ModelTrainer
+{
+    [TestClass]
+    public class MainViewModel
+    {
+        [TestMethod]
+        [ExpectedException(typeof(NullObjectException))]
+        public void ModelTrainer_NullTest()
+        {
+            var model = new MLIDS.ModelTrainer.ViewModels.MainViewModel();
+
+            model.StartAction();
+
+            model.StopAction();
+        }
+    }
+}
