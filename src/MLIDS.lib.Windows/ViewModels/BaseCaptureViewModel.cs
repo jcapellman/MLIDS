ef55515da53211a58b38ea075d9828dc50c8c0e4
Jarred Capellman <jcapellman@hotmail.com>
2020-05-05 19:17:41 -0500

    Change: Moved the Packet Processing functionality into the BaseCaptureMainViewModel class to avoid DRY violations

--- src/MLIDS.lib.Windows/ViewModels/BaseCaptureViewModel.cs
+++ src/MLIDS.lib.Windows/ViewModels/BaseCaptureViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 
+using MLIDS.lib.ML.Objects;
+
 using SharpPcap;
 using SharpPcap.Npcap;
 
@@ -7,6 +9,8 @@ namespace MLIDS.lib.Windows.ViewModels
 {
     public abstract class BaseCaptureMainViewModel : BaseViewModel
     {
+        private static readonly NLog.Logger Log = NLog.LogManager.GetCurrentClassLogger();
+
         private bool _chkBxSaveEnabled;
 
         public bool ChkBxSaveEnabled
@@ -61,11 +65,39 @@ namespace MLIDS.lib.Windows.ViewModels
             ChkBxSaveEnabled = true;
         }
 
-        public abstract void PacketProcessing(CaptureEventArgs e);
+        public abstract void PacketProcessing(PayloadItem payloadItem);
 
         private void Device_OnPacketArrival(object sender, CaptureEventArgs e)
         {
-            PacketProcessing(e);
+            if (e == null)
+            {
+                Log.Error("MainViewModel::PacketProcessing - e is null");
+
+                throw new ArgumentNullException(nameof(e));
+            }
+
+            System.Windows.Application.Current.Dispatcher.Invoke(delegate
+            {
+                var packet = Packet.ParsePacket(e.Packet.LinkLayerType, e.Packet.Data);
+
+                if (!packet.HasPayloadPacket)
+                {
+                    Log.Info("Packet has no payload");
+
+                    return;
+                }
+
+                var packetItem = GetPacket(packet, false);
+
+                if (packetItem == null)
+                {
+                    Log.Info("PacketItem was null");
+
+                    return;
+                }
+
+                PacketProcessing(packetItem);
+            });
         }
     }
 }
