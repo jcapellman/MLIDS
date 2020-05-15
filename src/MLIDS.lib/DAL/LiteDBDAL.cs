63ba7fe119f376d1ff34ac1b9c4c286eb028e923
Jarred Capellman <jcapellman@hotmail.com>
2020-05-14 19:26:05 -0500

    Change: Implemented Async Tasks for the LiteDBDAL class

--- src/MLIDS.lib/DAL/LiteDBDAL.cs
+++ src/MLIDS.lib/DAL/LiteDBDAL.cs
@@ -17,26 +17,32 @@ namespace MLIDS.lib.DAL
 
         public override Task<List<PayloadItem>> GetHostPacketsAsync(string hostName)
         {
-            using (var db = new LiteDatabase(_connectionString))
+            Task.Run(() =>
             {
+                using var db = new LiteDatabase(_connectionString);
+
                 return db.GetCollection<PayloadItem>().Find(a => a.HostName == hostName).ToList();
-            }
+            });
         }
 
         public override Task<List<PayloadItem>> QueryPacketsAsync(Expression<Func<PayloadItem, bool>> queryExpression)
         {
-            using (var db = new LiteDatabase(_connectionString))
+            Task.Run(() =>
             {
+                using var db = new LiteDatabase(_connectionString);
+
                 return db.GetCollection<PayloadItem>().Find(queryExpression).ToList();
-            }
+            });
         }
 
         public override Task<bool> WritePacketAsync(PayloadItem packet)
         {
-            using (var db = new LiteDatabase(_connectionString))
+            Task.Run(() =>
             {
+                using var db = new LiteDatabase(_connectionString);
+
                 return db.GetCollection<PayloadItem>().Insert(packet) > 0;
-            }
+            });
         }
     }
 }
