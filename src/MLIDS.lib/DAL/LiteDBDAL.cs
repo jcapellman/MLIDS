c874418d0e8ba17d331e4cf4f851794294fd2d6b
Jarred Capellman <jcapellman@hotmail.com>
2020-05-15 18:43:43 -0500

    Change: Expression Body usage in the LiteDBDAL

--- src/MLIDS.lib/DAL/LiteDBDAL.cs
+++ src/MLIDS.lib/DAL/LiteDBDAL.cs
@@ -21,34 +21,28 @@ namespace MLIDS.lib.DAL
             _connectionString = settings.DAL_FileName;
         }
 
-        public override Task<List<PayloadItem>> GetHostPacketsAsync(string hostName)
-        {
-            return Task.Run(() =>
+        public override Task<List<PayloadItem>> GetHostPacketsAsync(string hostName) => 
+            Task.Run(() =>
             {
                 using var db = new LiteDatabase(_connectionString);
 
                 return db.GetCollection<PayloadItem>().Find(a => a.HostName == hostName).ToList();
             });
-        }
 
-        public override Task<List<PayloadItem>> QueryPacketsAsync(Expression<Func<PayloadItem, bool>> queryExpression)
-        {
-            return Task.Run(() =>
+        public override Task<List<PayloadItem>> QueryPacketsAsync(Expression<Func<PayloadItem, bool>> queryExpression) => 
+            Task.Run(() =>
             {
                 using var db = new LiteDatabase(_connectionString);
 
                 return db.GetCollection<PayloadItem>().Find(queryExpression).ToList();
             });
-        }
 
-        public override Task<bool> WritePacketAsync(PayloadItem packet)
-        {
-            return Task.Run(() =>
+        public override Task<bool> WritePacketAsync(PayloadItem packet) => 
+            Task.Run(() =>
             {
                 using var db = new LiteDatabase(_connectionString);
 
                 return db.GetCollection<PayloadItem>().Insert(packet) > 0;
             });
-        }
     }
 }
