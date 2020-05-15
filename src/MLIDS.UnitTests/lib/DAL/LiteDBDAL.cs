3b11e5cf040310b416c41d5aa31ea2278b2c6d94
Jarred Capellman <jcapellman@hotmail.com>
2020-05-15 18:52:35 -0500

    Added: LiteDBDAL Unit Test class

---
+++ src/MLIDS.UnitTests/lib/DAL/LiteDBDAL.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MLIDS.UnitTests.lib.DAL
+{
+    [TestClass]
+    public class LiteDBDAL
+    {
+        [TestMethod]
+        public async Task LiteDBDAL_NullTestAsync()
+        {
+            var litedb = new MLIDS.lib.DAL.LiteDBDAL();
+
+            await litedb.GetHostPacketsAsync(null);
+        }
+
+        [TestMethod]
+        public async Task LiteDBDAL_StringTestAsync()
+        {
+            var litedb = new MLIDS.lib.DAL.LiteDBDAL();
+
+            await litedb.GetHostPacketsAsync("test");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public async Task LiteDBDAL_WriteNullTestAsync()
+        {
+            var litedb = new MLIDS.lib.DAL.LiteDBDAL();
+
+            await litedb.WritePacketAsync(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public async Task LiteDBDAL_QueryNullTestAsync()
+        {
+            var litedb = new MLIDS.lib.DAL.LiteDBDAL();
+
+            await litedb.QueryPacketsAsync(null);
+        }
+
+        [TestMethod]
+        public async Task LiteDBDAL_QueryEmptyTestAsync()
+        {
+            var litedb = new MLIDS.lib.DAL.LiteDBDAL();
+
+            var result = await litedb.QueryPacketsAsync(a => a.Label);
+
+            Assert.IsTrue(result.Any());
+        }
+    }
+}
