5d7a300c2e3a13ebb4b1770f1c05c6cc4c4dd0f0
Jarred Capellman <jcapellman@hotmail.com>
2020-05-15 19:35:52 -0500

    Added: JsonFileDAL class

---
+++ src/MLIDS.UnitTests/lib/DAL/JsonFileDAL.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MLIDS.UnitTests.lib.DAL
+{
+    [TestClass]
+    public class JsonDAL
+    {
+        [TestMethod]
+        public async Task LiteDBDAL_NullTestAsync()
+        {
+            var jsonDal = new MLIDS.lib.DAL.JsonFileDAL();
+
+            await jsonDal.GetHostPacketsAsync(null);
+        }
+
+        [TestMethod]
+        public async Task LiteDBDAL_StringTestAsync()
+        {
+            var jsonDal = new MLIDS.lib.DAL.JsonFileDAL();
+
+            await jsonDal.GetHostPacketsAsync("test");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public async Task LiteDBDAL_WriteNullTestAsync()
+        {
+            var jsonDal = new MLIDS.lib.DAL.JsonFileDAL();
+
+            await jsonDal.WritePacketAsync(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public async Task LiteDBDAL_QueryNullTestAsync()
+        {
+            var jsonDal = new MLIDS.lib.DAL.JsonFileDAL();
+
+            await jsonDal.QueryPacketsAsync(null);
+        }
+
+        [TestMethod]
+        public async Task LiteDBDAL_QueryEmptyTestAsync()
+        {
+            var jsonDal = new MLIDS.lib.DAL.JsonFileDAL();
+
+            var result = await jsonDal.QueryPacketsAsync(a => a.Label);
+
+            Assert.IsTrue(!result.Any());
+        }
+    }
+}
