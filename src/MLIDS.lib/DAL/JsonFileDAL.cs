222af3ae6c3397e3502d8d191c7961236a65d317
Jarred Capellman <jcapellman@hotmail.com>
2020-05-15 19:44:00 -0500

    Added: Error handling in the JsonFileDAL class

--- src/MLIDS.lib/DAL/JsonFileDAL.cs
+++ src/MLIDS.lib/DAL/JsonFileDAL.cs
@@ -6,7 +6,6 @@ using System.Linq.Expressions;
 using System.Text.Json;
 using System.Threading.Tasks;
 
-using MLIDS.lib.Common;
 using MLIDS.lib.Containers;
 using MLIDS.lib.DAL.Base;
 using MLIDS.lib.ML.Objects;
@@ -15,9 +14,13 @@ namespace MLIDS.lib.DAL
 {
     public class JsonFileDAL : BaseDAL
     {
+        private static readonly NLog.Logger Log = NLog.LogManager.GetCurrentClassLogger();
+
+        private const string DEFAULT_JSON_FILE = "db.json";
+
         private string _fileName;
 
-        public JsonFileDAL(string fileName = Constants.DAL_FileName)
+        public JsonFileDAL(string fileName = DEFAULT_JSON_FILE)
         {
             _fileName = fileName;
         }
@@ -26,6 +29,11 @@ namespace MLIDS.lib.DAL
 
         public override async Task<List<PayloadItem>> GetHostPacketsAsync(string hostName)
         {
+            if (!File.Exists(_fileName))
+            {
+                return new List<PayloadItem>();
+            }
+
             using var fs = File.OpenRead(_fileName);
 
             var data = await JsonSerializer.DeserializeAsync<List<PayloadItem>>(fs);
@@ -35,6 +43,18 @@ namespace MLIDS.lib.DAL
         
         public override async Task<List<PayloadItem>> QueryPacketsAsync(Expression<Func<PayloadItem, bool>> queryExpression)
         {
+            if (queryExpression == null)
+            {
+                Log.Error($"JsonFileDAL::QueryPacketsAsync - Query Expression was null");
+
+                throw new ArgumentNullException(nameof(queryExpression));
+            }
+
+            if (!File.Exists(_fileName))
+            {
+                return new List<PayloadItem>();
+            }
+
             using var fs = File.OpenRead(_fileName);
 
             var data = await JsonSerializer.DeserializeAsync<List<PayloadItem>>(fs);
@@ -44,9 +64,21 @@ namespace MLIDS.lib.DAL
 
         public override async Task<bool> WritePacketAsync(PayloadItem packet)
         {
-            using var fs = File.OpenRead(_fileName);
+            if (packet == null)
+            {
+                Log.Error($"JsonFileDAL::WritePacketAsync - packet was null");
 
-            var data = await JsonSerializer.DeserializeAsync<List<PayloadItem>>(fs);
+                throw new ArgumentNullException(nameof(packet));
+            }
+
+            var data = new List<PayloadItem>();
+
+            if (File.Exists(_fileName))
+            {
+                using var fs = File.OpenRead(_fileName);
+
+                data = await JsonSerializer.DeserializeAsync<List<PayloadItem>>(fs);
+            }
 
             data.Add(packet);
 
