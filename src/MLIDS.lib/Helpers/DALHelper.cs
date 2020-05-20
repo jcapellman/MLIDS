25fe109a89fdd6785d21fc1c8ace45b1d9362ec1
Jarred Capellman <jcapellman@hotmail.com>
2020-05-19 19:56:44 -0500

    Added: DALHelper Class

---
+++ src/MLIDS.lib/Helpers/DALHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using MLIDS.lib.DAL.Base;
+
+namespace MLIDS.lib.Helpers
+{
+    public static class DALHelper
+    {
+        public static List<BaseDAL> GetAvailableDALs() => 
+            typeof(DALHelper).Assembly.GetTypes().Where(a => typeof(BaseDAL) == 
+            a.BaseType && !a.IsAbstract).Select(b => (BaseDAL)Activator.CreateInstance(b)).ToList();
+    }
+}
