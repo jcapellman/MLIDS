896ed378efad2229826011a2ec9d5f47a8a375cb
Jarred Capellman <jcapellman@hotmail.com>
2020-05-22 18:02:20 -0500

    Added: settingsItem to the BaseDAL constructor and property

--- src/MLIDS.lib/DAL/Base/BaseDAL.cs
+++ src/MLIDS.lib/DAL/Base/BaseDAL.cs
@@ -1,4 +1,5 @@
-﻿using MLIDS.lib.ML.Objects;
+﻿using MLIDS.lib.Containers;
+using MLIDS.lib.ML.Objects;
 
 using System;
 using System.Collections.Generic;
@@ -8,8 +9,15 @@ namespace MLIDS.lib.DAL.Base
 {
     public abstract class BaseDAL
     {
-        public abstract string Description { get; }
+        protected SettingsItem settingsItem;
 
+        protected BaseDAL(SettingsItem settingsItem)
+        {
+            this.settingsItem = settingsItem;
+        }
+
+        public abstract string Description { get; }
+        
         public abstract Task<bool> WritePacketAsync(PayloadItem packet);
 
         public abstract Task<List<PayloadItem>> GetHostPacketsAsync(string hostName);
