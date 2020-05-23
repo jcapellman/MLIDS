9f46121d1c513a5934a4152b6503309a52de28b5
Jarred Capellman <jcapellman@hotmail.com>
2020-05-23 18:49:21 -0500

    Added: EmptyDAL class

---
+++ src/MLIDS.lib/DAL/EmptyDAL.cs
@@ -0,0 +1,37 @@
+﻿using MLIDS.lib.Containers;
+using MLIDS.lib.DAL.Base;
+using MLIDS.lib.ML.Objects;
+
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace MLIDS.lib.DAL
+{
+    public class EmptyDAL : BaseDAL
+    {
+        public override string Description => "--Select a Data Layer--";
+
+        public override bool IsEmpty => true;
+
+        public EmptyDAL(SettingsItem settings) : base(settings) { }
+
+        public override Task<List<PayloadItem>> GetHostPacketsAsync(string hostName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Initialize() => false;
+
+        public override Task<List<PayloadItem>> QueryPacketsAsync(Expression<Func<PayloadItem, bool>> queryExpression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<bool> WritePacketAsync(PayloadItem packet)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
