fdada1c259e7a24dfccf78ac764e93e7ec9c4a01
Jarred Capellman <jcapellman@hotmail.com>
2020-06-07 10:02:07 -0500

    Added: Placeholder to do statements for CSV -> T conversion

--- src/MLIDS.lib/Extensions/CSV.cs
+++ src/MLIDS.lib/Extensions/CSV.cs
@@ -6,5 +6,12 @@ namespace MLIDS.lib.Extensions
     {
         public static string ToCSV<T>(this object obj) =>
             string.Join(',', typeof(T).GetProperties().Select(property => property.GetValue(obj)?.ToString()));
+
+        public static T FromCSV<T>(this string line)
+        {
+            // Activator
+            // Iterate through properties and set value
+            // return T
+        }
     }
 }
