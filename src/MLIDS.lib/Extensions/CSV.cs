9fbb6b48d9463bea9cc3f0c32f5fe447f3347e9f
Jarred Capellman <jcapellman@hotmail.com>
2020-06-07 14:24:26 -0500

    Added: Extension method to convert from CSV to T #40

--- src/MLIDS.lib/Extensions/CSV.cs
+++ src/MLIDS.lib/Extensions/CSV.cs
@@ -1,17 +1,33 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 
 namespace MLIDS.lib.Extensions
 {
     public static class CSV
     {
+        private const char SEPARATOR = ',';
+
         public static string ToCSV<T>(this object obj) =>
-            string.Join(',', typeof(T).GetProperties().Select(property => property.GetValue(obj)?.ToString()));
+            string.Join(SEPARATOR, typeof(T).GetProperties().OrderBy(a => a.Name).Select(property => property.GetValue(obj)?.ToString()));
 
         public static T FromCSV<T>(this string line)
         {
-            // Activator
-            // Iterate through properties and set value
-            // return T
+            T obj = Activator.CreateInstance<T>();
+
+            var properties = obj.GetType().GetProperties().OrderBy(a => a.Name);
+
+            var lineProps = line.Split(SEPARATOR);
+
+            var idx = 0;
+
+            foreach (var prop in properties)
+            {
+                prop.SetValue(obj, lineProps[idx]);
+
+                idx++;    
+            }
+
+            return obj;
         }
     }
 }
