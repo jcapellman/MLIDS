329faf009898b9edaa4fd7706dd7afc7ea29d958
Jarred Capellman <jcapellman@hotmail.com>
2020-06-14 17:21:02 -0500

    Added: Additional abstract members and methods to the BaseVector class #48

--- src/MLIDS.Scripter.lib/Vectors/Base/BaseVector.cs
+++ src/MLIDS.Scripter.lib/Vectors/Base/BaseVector.cs
@@ -4,6 +4,10 @@ namespace MLIDS.Scripter.lib
 {
     public abstract class BaseVector
     {
+        public abstract int NumRequiredArguments { get; }
+
         public abstract ScriptVectors ScriptType { get; }
+
+        public abstract void Execute(string[] arguments);
     }
 }
