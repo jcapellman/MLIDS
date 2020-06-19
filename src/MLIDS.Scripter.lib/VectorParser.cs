813ae07ac6adae5d05b86102ed16ed19f1386201
Jarred Capellman <jcapellman@hotmail.com>
2020-06-19 16:37:16 -0500

    Change: Initial work on the JSON -> Class Objects

--- src/MLIDS.Scripter.lib/VectorParser.cs
+++ src/MLIDS.Scripter.lib/VectorParser.cs
@@ -1,20 +1,25 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace MLIDS.Scripter.lib
 {
     public class VectorParser
     {
-        public List<BaseVector> ParseScript(string fileName)
+        public async Task<List<BaseVector>> ParseScriptAsync(string fileName)
         {
             if (!File.Exists(fileName))
             {
                 throw new FileNotFoundException($"VectorParser::ParseScript - File not found {fileName}");
             }
 
+            using (var fs = File.OpenRead(fileName))
+            {
+                var scriptVectors = await JsonSerializer.DeserializeAsync<List<BaseVector>>(fs);
 
-
-            return new List<BaseVector>();
+                return scriptVectors;
+            }
         }
     }
 }
