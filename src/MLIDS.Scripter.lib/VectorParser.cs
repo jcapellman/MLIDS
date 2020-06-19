38acc5d0b67bc03594df167aed390578af7909d2
Jarred Capellman <jcapellman@hotmail.com>
2020-06-18 19:51:03 -0500

    Added: Initial work on the VectorParser class

---
+++ src/MLIDS.Scripter.lib/VectorParser.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace MLIDS.Scripter.lib
+{
+    public class VectorParser
+    {
+        public List<BaseVector> ParseScript(string fileName)
+        {
+            if (!File.Exists(fileName))
+            {
+                throw new FileNotFoundException($"VectorParser::ParseScript - File not found {fileName}");
+            }
+
+
+
+            return new List<BaseVector>();
+        }
+    }
+}
