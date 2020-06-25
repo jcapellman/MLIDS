690627d3a363dbbebc4545f8005fcb36f2bf42f9
Jarred Capellman <jcapellman@hotmail.com>
2020-06-24 19:35:07 -0500

    Added: Initial work on the Menu

--- src/MLIDS.ScriptEditor/MainWindow.xaml.cs
+++ src/MLIDS.ScriptEditor/MainWindow.xaml.cs
@@ -8,5 +8,35 @@ namespace MLIDS.ScriptEditor
         {
             InitializeComponent();
         }
+
+        private void btnNew_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnOpen_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSaveAs_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAbout_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
