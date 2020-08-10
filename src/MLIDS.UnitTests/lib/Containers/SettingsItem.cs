d41cd5ceeccd9e648c42df116f884588a22e8996
Jarred Capellman <jcapellman@hotmail.com>
2020-08-09 21:05:23 -0500

    Added: Unit Test coverage for the Save Method

--- src/MLIDS.UnitTests/lib/Containers/SettingsItem.cs
+++ src/MLIDS.UnitTests/lib/Containers/SettingsItem.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.Json;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -12,6 +13,28 @@ namespace MLIDS.UnitTests.lib.Containers
         public void SettingsItem_NullTest()
         {
             MLIDS.lib.Containers.SettingsItem.Load(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SettingsItem_NullSaveTest()
+        {
+            MLIDS.lib.Containers.SettingsItem.Save(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(JsonException))]
+        public void SettingsItem_BadJSONSaveTest()
+        {
+            MLIDS.lib.Containers.SettingsItem.Save("bad");
+        }
+
+        [TestMethod]
+        public void SettingsItem_GoodJSONSaveTest()
+        {
+            var setting = MLIDS.lib.Containers.SettingsItem.Save("{\"DAL_HostIP\":\"192.168.0.1\",\"DAL_HostPort\":0,\"DAL_FileName\":\"\"}");
+
+            Assert.AreEqual("192.168.0.1", setting.DAL_HostIP);
         }
     }
 }
