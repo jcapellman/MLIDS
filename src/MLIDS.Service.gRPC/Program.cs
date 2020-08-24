a6bbbe13641930dd901f0dad9b1bef954e910951
Jarred Capellman <jcapellman@hotmail.com>
2020-08-23 19:14:45 -0500

    Added: MongoDB Service to the PacketStorageService

--- src/MLIDS.Service.gRPC/Program.cs
+++ src/MLIDS.Service.gRPC/Program.cs
@@ -10,8 +10,6 @@ namespace MLIDS.Service.gRPC
             CreateHostBuilder(args).Build().Run();
         }
 
-        // Additional configuration is required to successfully run gRPC on macOS.
-        // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
--- src/MLIDS.Service.gRPC/Services/PacketStorageService.cs
+++ src/MLIDS.Service.gRPC/Services/PacketStorageService.cs
@@ -1,8 +1,12 @@
+using System.Text.Json;
 using System.Threading.Tasks;
 
 using Grpc.Core;
 
 using Microsoft.Extensions.Logging;
+
+using MLIDS.lib.DAL;
+using MLIDS.lib.ML.Objects;
 
 using MLIDS.Service.gRPC.Protos;
 
@@ -11,18 +15,23 @@ namespace MLIDS.Service.gRPC.Services
     public class PacketStorageService : PacketStorage.PacketStorageBase
     {
         private readonly ILogger<PacketStorageService> _logger;
+        private MongoDAL _dbService = new MongoDAL(new lib.Containers.SettingsItem());
 
         public PacketStorageService(ILogger<PacketStorageService> logger)
         {
             _logger = logger;
+
+            _dbService.Initialize();
         }
 
-        public override Task<PacketStorageReply> WritePacket(PacketStorageRequest request, ServerCallContext context)
+        public override async Task<PacketStorageReply> WritePacket(PacketStorageRequest request, ServerCallContext context)
         {
-            return Task.FromResult(new PacketStorageReply
+            var result = await _dbService.WritePacketAsync(JsonSerializer.Deserialize<PayloadItem>(request.JSON));
+
+            return new PacketStorageReply
             {
-                Success = true
-            });
+                Success = result
+            };
         }
     }
 }
