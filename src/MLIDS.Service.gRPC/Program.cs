642a98b587a6ab89ea017015755d023e075ee9a8
Jarred Capellman <jcapellman@hotmail.com>
2020-08-24 18:58:44 -0500

    Added: NLog to the MLIDS.Service.gRPC

--- src/MLIDS.Service.gRPC/Program.cs
+++ src/MLIDS.Service.gRPC/Program.cs
@@ -1,4 +1,7 @@
+using NLog.Web;
+
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Hosting;
 
 namespace MLIDS.Service.gRPC
@@ -7,6 +10,9 @@ namespace MLIDS.Service.gRPC
     {
         public static void Main(string[] args)
         {
+            var logger = NLog.Web.NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+
+            logger.Debug("init main");
             CreateHostBuilder(args).Build().Run();
         }
 
@@ -15,6 +21,12 @@ namespace MLIDS.Service.gRPC
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Debug);
+                })
+                .UseNLog();
     }
 }
