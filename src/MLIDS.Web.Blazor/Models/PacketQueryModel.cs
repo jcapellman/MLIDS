9c428a32a642c3d8421fbf65139b6c92c7ffc4c8
Jarred Capellman <jcapellman@hotmail.com>
2020-09-09 21:33:04 -0500

    Added: Default PacketQueryModel and Labels

--- src/MLIDS.Web.Blazor/Models/PacketQueryModel.cs
+++ src/MLIDS.Web.Blazor/Models/PacketQueryModel.cs
@@ -7,5 +7,11 @@ namespace MLIDS.Web.Blazor.Models
         public DateTime StartTime { get; set; }
 
         public DateTime EndTime { get; set; }
+
+        public PacketQueryModel()
+        {
+            StartTime = DateTime.Now.AddDays(-7);
+            EndTime = DateTime.Now;
+        }
     }
 }
