abaa18c35ea1edeb1f1ee50f8f97f5f2943c9895
Jarred Capellman <jcapellman@hotmail.com>
2022-04-11 08:58:11 -0500

    Fix: Properly handling wpcap not being installed

--- src/MLIDS.DataCapture/App.xaml.cs
+++ src/MLIDS.DataCapture/App.xaml.cs
@@ -1,21 +1,8 @@
-﻿using System;
-using System.Windows;
+﻿using System.Windows;
 
 namespace MLIDS.DataCapture
 {
     public partial class App : Application
     {
-        public App()
-        {
-            this.DispatcherUnhandledException += App_DispatcherUnhandledException;
-        }
-
-        private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
-        {
-            if (e.Exception.GetType() == typeof(DllNotFoundException))
-            {
-                MessageBox.Show("WinPcap is not installed");
-            }
-        }
     }
 }
--- src/MLIDS.DataCapture/MainWindow.xaml.cs
+++ src/MLIDS.DataCapture/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using MLIDS.lib.Windows;
+﻿using System;
+using System.Windows;
+
+using MLIDS.DataCapture.ViewModels;
+using MLIDS.lib.Windows;
 
 namespace MLIDS.DataCapture
 {
@@ -7,6 +11,19 @@ namespace MLIDS.DataCapture
         public MainWindow()
         {
             InitializeComponent();
+
+            try
+            {
+                DataContext = new MainViewModel();
+            } catch (DllNotFoundException e)
+            {
+                if (e.Message.Contains("wpcap"))
+                {
+                    MessageBox.Show("WinPcap is not installed and is required to run the application - exiting");
+
+                    Environment.Exit(1);
+                }                
+            }
         }
     }
 }
