f74157449f11319f0a18d98791e8c54b3e514b47
Jarred Capellman <jcapellman@hotmail.com>
2022-05-02 06:31:21 -0500

    Added: Initial work on the Unit Tests for the Windows Lib

---
+++ src/MLIDS.UnitTests/lib.Windows/ViewModels/BaseViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System.Linq;
+
+namespace MLIDS.UnitTests.lib.Windows.ViewModels
+{
+    [TestClass]
+    public class BaseViewModel
+    {
+        public class TestViewModel : MLIDS.lib.Windows.ViewModels.BaseViewModel
+        {
+            public override void StartAction()
+            {
+                
+            }
+
+            public override void StartButtonEnablement()
+            {
+                
+            }
+
+            public override void StopAction()
+            {
+                
+            }
+        }
+
+        [TestMethod]
+        public void BaseViewModel_EmptyTest()
+        {
+            var viewModel = new TestViewModel();
+
+            Assert.IsTrue(viewModel.DataLayers.Any());
+            Assert.IsNotNull(viewModel.SelectedDataLayer);
+        }
+    }
+}
